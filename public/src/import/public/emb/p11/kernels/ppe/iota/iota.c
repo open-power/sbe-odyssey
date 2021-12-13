26b738b5ed64e3caa24be2675ba77ad3753df7b8
Douglas Gilbert <dgilbert@us.ibm.com>
2021-12-13 10:45:27 -0600

    Add PPE Kernels
    Change-Id: Ia659c6c5215fa41417db9d77993c6c13d32d90e6
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/125901
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Doug Gilbert <dgilbert@us.ibm.com>
    Tested-by: Doug Gilbert <dgilbert@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/127103
    Reviewed-by: Sumit Kumar <sumit_kumar@in.ibm.com>

---
+++ public/src/import/public/emb/p11/kernels/ppe/iota/iota.c
@@ -0,0 +1,259 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: public/src/import/public/emb/p11/kernels/ppe/iota/iota.c $    */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2021,2022                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+#include "iota.h"
+#include "iota_ppe42.h"
+#include "iota_panic_codes.h"
+
+// Force all kernel variables into .sdata
+// Order controlled by linker script
+union
+{
+    uint64_t value;
+    struct
+    {
+        uint32_t upper;
+        uint32_t lower;
+    };
+} ppe42_64bit_timebase __attribute__((section(".sdata.ppe42_64bit_timebase"))) = {0};
+
+uint32_t g_iota_intr_depth_count __attribute__((section(".sdata.g_iota_intr_depth_count"))) = 0;
+
+uint64_t g_iota_execution_stack[IOTA_STACK_DWORD_SIZE] __attribute__((aligned(8))) =
+{
+    [ 0 ... (IOTA_STACK_DWORD_SIZE - 1) ] = IOTA_STACK_PATTERN
+};
+uint64_t g_iota_execution_stack_end __attribute__((section(".data.g_iota_execution_stack_end"))) = 0;
+
+// Plugin default functions
+iotaTimerFuncPtr g_iota_dec_handler = IOTA_TIMER_HANDLER(__iota_halt);
+iotaTimerFuncPtr g_iota_fit_handler = IOTA_TIMER_HANDLER(__iota_halt);
+iotaTimerFuncPtr g_iota_machine_check_handler = IOTA_TIMER_HANDLER(__iota_halt);
+
+extern uint32_t G_LOCAL_TIMEBASE_REGISTER __attribute__((section(".sdata")));
+uint32_t G_LOCAL_TIMEBASE_REGISTER = LOCAL_TIMEBASE_REGISTER;
+
+#if IOTA_IDLE_TASKS_ENABLE
+void _iota_evaluate_idle_tasks()
+{
+    // Iterate over all idle tasks
+    uint32_t idle_task_idx = 0;
+
+    for(; idle_task_idx < g_iota_idle_task_list_size; ++idle_task_idx)
+    {
+        // Enabled tasks get executed
+        if(g_iota_idle_task_list[idle_task_idx].state >= IOTA_IDLE_ENABLED)
+        {
+            if(g_iota_idle_task_list[idle_task_idx].function == IOTA_NO_TASK)
+            {
+                // Tried to execute a NULLPTR task
+                iota_dead(IOTA_NULLPTR_TASK);
+            }
+
+            g_iota_idle_task_list[idle_task_idx].function(idle_task_idx, 0);
+
+#if IOTA_AUTO_DISABLE_IDLE_TASKS
+            uint32_t ctx = mfmsr();
+            wrteei(0);
+
+            g_iota_idle_task_list[idle_task_idx].state = IOTA_IDLE_DISABLED;
+
+            mtmsr(ctx);
+#endif
+        }
+    }
+}
+#endif  // IOTA_IDLE_TASKS_ENABLE
+
+#if IOTA_INIT_TASKS_ENABLE
+void _iota_run_init_tasks()
+{
+    int init_task_idx = 0;
+
+    for(; init_task_idx < g_iota_init_task_list_size; ++init_task_idx)
+    {
+        if(g_iota_init_task_list[init_task_idx] == IOTA_NO_TASK)
+        {
+            iota_dead(IOTA_NULLPTR_TASK);
+        }
+
+        g_iota_init_task_list[init_task_idx]();
+    }
+}
+#endif
+
+extern void main(void);
+
+void _iota_machine_check_handler()
+{
+#if ENABLE_MACHINE_CHECK_HANDLER
+    g_iota_machine_check_handler();
+#else
+    asm volatile ("tw 31, 0, 0");
+#endif
+}
+
+void _iota_boot()
+{
+    mtdbcr(PPE42_DBCR_INITIAL);
+
+    // Disable timer interrupts
+    mttcr(0);
+
+    g_iota_intr_depth_count = 0;
+
+    mtmsr(IOTA_INITIAL_MSR);
+    // ext intr should be masked at this point
+
+    __hwmacro_setup(); // Configures external interrupt registers
+
+    main();
+}
+
+void iota_run()
+{
+    mtmsr(IOTA_DEFAULT_MSR);
+#if IOTA_INIT_TASKS_ENABLE
+    _iota_run_init_tasks();
+#endif
+
+    wrteei(1);
+
+    while(1)
+    {
+        // Put PPE into wait mode (idle)
+        ppe_idle();
+        // Execution continues here after an external or FIT interrupt
+#if IOTA_IDLE_TASKS_ENABLE
+        // Run any enabled idle tasks
+        _iota_evaluate_idle_tasks();
+#endif
+    }
+}
+
+#if IOTA_IDLE_TASKS_ENABLE
+void iota_set_idle_task_state(uint32_t state, uint32_t idle_task_idx)
+{
+    if(idle_task_idx < g_iota_idle_task_list_size)
+    {
+        // Ignore any task entries which have no task assigned...
+        if(g_iota_idle_task_list[idle_task_idx].function != IOTA_NO_TASK)
+        {
+            // Setting/changing an idle task enable needs to be done atomically
+            uint32_t ctx = mfmsr();
+            wrteei(0);
+
+            g_iota_idle_task_list[idle_task_idx].state = state;
+
+            mtmsr(ctx);
+        }
+    }
+}
+#endif
+
+void _iota_schedule(uint32_t schedule_reason)
+{
+    ++g_iota_intr_depth_count;
+    // stack check
+    uint32_t stack_ptr;
+    asm volatile ("mr %0,1":"=r"(stack_ptr)::);
+
+    if(stack_ptr <= (uint32_t)g_iota_execution_stack)
+    {
+        iota_dead(IOTA_MACHINE_STATE_STACK_OVERFLOW);
+    }
+
+    // Call appropriate interrupt handler here
+    switch(schedule_reason)
+    {
+        case _IOTA_SCHEDULE_REASON_EXT:
+            {
+                uint32_t task_idx = __ext_irq_handler();
+
+                if(g_iota_task_list[task_idx] != IOTA_NO_TASK)
+                {
+                    mtmsr(IOTA_DEFAULT_MSR);
+                    g_iota_task_list[task_idx]();
+                    // Interrupts are disabled once the task returns
+                    wrteei(0);
+                    __ext_irq_resume();
+                }
+                else
+                {
+                    iota_dead(IOTA_NULLPTR_TASK);
+                }
+            }
+
+            break;
+
+#if ENABLE_DEC_TIMER
+
+        case _IOTA_SCHEDULE_REASON_DEC:
+            mtspr(SPRN_TSR, TSR_DIS); // write 1 to clear
+            g_iota_dec_handler();
+            break;
+#endif
+
+#if ENABLE_FIT_TIMER
+
+        case _IOTA_SCHEDULE_REASON_FIT:
+            mtspr(SPRN_TSR, TSR_FIS); // write 1 to clear
+            g_iota_fit_handler();
+            break;
+#endif
+
+    }
+
+    wrteei(0);
+
+    // Check execution stack integrity
+    if(g_iota_execution_stack[0] != IOTA_STACK_PATTERN)
+    {
+        iota_dead(IOTA_EXECUTION_STACK_OVERFLOW);
+    }
+
+    if(g_iota_intr_depth_count != 0)
+    {
+        g_iota_intr_depth_count--;
+    }
+    else
+    {
+        iota_dead(IOTA_MACHINE_STATE_STACK_UNDERFLOW);
+    }
+}
+
+// Assumption is that this is called more frequently than
+// the local time base hw register rolls over.
+uint64_t pk_timebase_get()
+{
+    uint32_t hw_timebase = in64(G_LOCAL_TIMEBASE_REGISTER) >> 32;
+
+    if(ppe42_64bit_timebase.lower > hw_timebase)
+    {
+        ++ppe42_64bit_timebase.upper;
+    }
+
+    ppe42_64bit_timebase.lower = hw_timebase;
+    return ppe42_64bit_timebase.value;
+}
---
+++ public/src/import/public/emb/p11/kernels/ppe/pk/test/pk_test.mk
@@ -0,0 +1,127 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: public/src/import/public/emb/p11/kernels/ppe/pk/test/pk_test.mk $ */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2021,2022                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+IMAGE:=pk_test
+
+# Use PPE toolchain
+$(IMAGE)_TARGET=PPE
+
+# Use GPE Bolton (can be gpe|tapce|exppe|vanilla )
+#   gpe - use OCB (pm occ complex)
+#   standard - use 64 bit irq (qme/sbe)
+#   vanilla - use internal timebase only(DEC), no irq  (others)
+_PPE_TYPE=gpe
+
+# Override default link script name of $(IMAGE).cmd
+$(IMAGE)_LINK_SCRIPT=link.ld
+
+# PK is highly configurable using compile time flags so using archive libs
+# is not a good idea where different PPE engines are supported in the same REPO
+# Only build desired parts of PK.
+
+$(call ADD_PPEIMAGE_SRCDIR,$(IMAGE),$(BASELIB_SRCDIR))
+
+# Include all PPE_TRACE objects
+include $(PPETRACE_SRCDIR)/pktracefiles.mk
+OBJS := $(PKTRACE_OBJECTS)
+$(call ADD_PPEIMAGE_SRCDIR,$(IMAGE),$(PPETRACE_SRCDIR)/)
+
+#Cherry pick desired objects from $(BASELIB_SRCDIR) and $(PK_SRCDIR)/ppe42
+OBJS += ppe42_gcc.o
+OBJS += ppe42_init.o
+OBJS += ppe42_core.o
+OBJS += ppe42_irq_core.o
+OBJS += ppe42_irq_init.o
+OBJS += ppe42_scom.o
+OBJS += eabi.o
+OBJS += ppe42_math.o
+OBJS += ppe42_boot.o
+OBJS += ppe42_timebase.o
+OBJS += ppe42_thread_init.o
+OBJS += ppe42_exceptions.o
+
+include $(PK_SRCDIR)/kernel/pkkernelfiles.mk
+OBJS += $(PK_OBJECTS)
+OBJS += $(PK_TIMER_OBJECTS)
+OBJS += $(PK_THREAD_OBJECTS)
+$(call ADD_PPEIMAGE_SRCDIR,$(IMAGE),$(PK_SRCDIR)/kernel)
+
+#include $(PK_SRCDIR)/ppe42/pkppe42files.mk
+#OBJS += $(PPE42_OBJECTS)
+#OBJS += $(PPE42_THREAD_OBJECTS)
+$(call ADD_PPEIMAGE_SRCDIR,$(IMAGE),$(PK_SRCDIR)/ppe42)
+
+# Include pk kernel
+include $(BOLTONLIB_SRCDIR)/$(_PPE_TYPE)/pk$(_PPE_TYPE)files.mk
+OBJS += $(GPE_OBJECTS)
+$(call ADD_PPEIMAGE_SRCDIR,$(IMAGE),$(BOLTONLIB_SRCDIR)/$(_PPE_TYPE))
+
+## memset string
+include $(OCC_SRCDIR)/commonlib/libcommonfiles.mk
+OBJS += $(LIBCOMMON_OBJECTS)
+$(call ADD_PPEIMAGE_SRCDIR,$(IMAGE),$(OCC_SRCDIR)/commonlib)
+
+## PM complex IPC support
+include $(OCC_SRCDIR)/occlib/liboccfiles.mk
+OBJS += $(LIBOCC_OBJECTS)
+$(call ADD_PPEIMAGE_SRCDIR,$(IMAGE),$(OCC_SRCDIR)/occlib)
+
+# Include target mk file  last!!
+# It's important that the final included *.mk is in the image target src dir
+# as it's tracked by the make macros
+include $(PK_SRCDIR)/test/pk_testfiles.mk
+OBJS += $(PKTEST_OBJECTS)
+
+$(IMAGE)_TRACE_HASH_PREFIX := $(shell echo $(IMAGE) | md5sum | cut -c1-4 \
+	| xargs -i printf "%d" 0x{})
+
+# Customize PK
+$(IMAGE)_COMMONFLAGS+= -DPK_TRACE_LEVEL=3
+$(IMAGE)_COMMONFLAGS+= -DPK_THREAD_SUPPORT=1
+$(IMAGE)_COMMONFLAGS+= -DPK_TRACE_SUPPORT=1
+$(IMAGE)_COMMONFLAGS+= -DUSE_PK_APP_CFG_H=1
+$(IMAGE)_COMMONFLAGS+= -D__PPE_PLAT
+$(IMAGE)_COMMONFLAGS+= -D__PK__=1
+$(IMAGE)_COMMONFLAGS+= -DPBASLVCTLN=0
+$(IMAGE)_COMMONFLAGS+= -DAPPCFG_OCC_INSTANCE_ID=1
+
+# add include paths
+$(call ADD_PPEIMAGE_INCDIR,$(IMAGE),\
+	$(PK_SRCDIR)/test \
+	$(PK_SRCDIR)/kernel \
+	$(PK_SRCDIR)/ppe42 \
+	$(PPETRACE_SRCDIR) \
+	$(BOLTONLIB_SRCDIR)/$(_PPE_TYPE) \
+	$(PMLIB_INCDIR)/registers \
+	$(OCC_SRCDIR)/occlib \
+	$(POWMANLIB_SRCDIR) \
+	$(PMLIB_INCDIR)/registers \
+	$(BOLTONLIB_SRCDIR)/$(_PPE_TYPE)\
+	)
+
+# linker flags
+$(IMAGE)_LDFLAGS=-e __system_reset -N -gc-sections -Bstatic
+
+
+$(call BUILD_PPEIMAGE)
