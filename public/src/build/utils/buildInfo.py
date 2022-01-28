39096fa32d19e3cb67a18a8ffd4aba33b0437035
akhis023 <akhis023@in.ibm.com>
2022-01-28 07:11:35 -0600

    Meson Build Infra for ODYSSEY
    -Build basic Odyssey srom,boot loader and sppe image.
    -Generate .bin,.out,.syms,.dis,.map & Stringfile for all images.
    -Generate .rpt file containing elf header & section's for all images
    -Provided DD Level option for build
    -Provided meson option's for user to build image of user choise.
     (srom,sppe,bldr)
    -Provided meson wraper tool to build repo
    -Updated .gitignore to ignore import dir
    -Updated README with latest build commands
    -Build ppe and pk code(ekb-src)
    -Initilize pk kernel and single thread for all images.
    -Generate build info genfile.
    -Build sha512,sha3,dilithium and ecdsa algo's
    -Modified parseandcutelf.py to support non xip package
    -Added ppe2fsp,vector
    
    Note:Linker Script used to generate images are basic.
         Linker and odysseylinks.H need to be modified as per design.
    
    Change-Id: I6278a7b40f4c590b8eb688ff5f62eba031b7271c
    Signed-off-by: akhis023 <akhis023@in.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/126910
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>
    Tested-by: RAJA DAS <rajadas2@in.ibm.com>

---
+++ public/src/build/utils/buildInfo.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: public/src/build/utils/buildInfo.py $
+#
+# OpenPOWER sbe Project
+#
+# Contributors Listed Below - COPYRIGHT 2016,2022
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+
+# This script will create header file sbe_build_info.H which will have
+# buld information required by SBE code.
+import os
+import sys
+
+def buildInfo():
+    buildInfoFileName = sys.argv[1]+"/sbe_build_info.H"
+    header = \
+"#ifndef SBE_BUILD_INFO_H  \n\
+#define SBE_BUILD_INFO_H  \n\n"
+
+    footer = "\n#endif  // SBE_BUILD_INFO_H"
+
+    if 'SBE_COMMIT_ID' in os.environ:
+        commitStr = os.environ['SBE_COMMIT_ID'][:8]
+    else:
+        commitStr = os.popen('git rev-parse --short=8 HEAD').read().rstrip()
+    try:
+        commitInt = int(commitStr, 16)
+    except:
+        print("Failed to get a valid SBE_COMMIT_ID")
+        sys.exit(1)
+
+    f = open( buildInfoFileName, 'w')
+
+    f.write(header)
+    f.write("//Define SBE Commit ID \n")
+    f.write("#define SBE_COMMIT_ID " + hex(commitInt) + "\n")
+    f.write(footer)
+    f.close()
+
+# Call buildInfo
+buildInfo()
---
+++ public/src/runtime/odyssey/sppe/build/linker/linker.cmd
@@ -0,0 +1,111 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: public/src/runtime/odyssey/sppe/build/linker/linker.cmd $     */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2022                        */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+// Need to do this so that elf32-powerpc is not modified!
+#undef powerpc
+
+//TODO:This is a basic linker.Update as per needs/design
+
+#include "odysseylink.H"
+#define INITIAL_STACK_SIZE 256
+#define BASE_LOADER_STACK_SIZE 65536
+
+OUTPUT_FORMAT(elf32-powerpc);
+
+MEMORY
+{
+ rom(rx)   : ORIGIN = SROM_ORIGIN, LENGTH = SROM_SIZE
+ sram(rw)  : ORIGIN = SRAM_ORIGIN, LENGTH = SRAM_SIZE
+}
+
+SECTIONS
+{
+    . = SROM_ORIGIN;
+
+    ////////////////////////////////
+    // Read-only Data
+    ////////////////////////////////
+
+    . = ALIGN(8);
+    _RODATA_SECTION_BASE = .;
+
+   .text . : { *(.text) } > rom
+   .data  . : { *(.data) } > rom
+
+    // SDA2 constant sections .sdata2 and .sbss2 must be adjacent to each
+    // other.  Our SDATA sections are small so we'll use strictly positive
+    // offsets.
+
+    _SDA2_BASE_ = .;
+   .sdata2 . : { *(.sdata2*) } > rom
+   .sbss2  . : { *(.sbss2*) } > rom
+
+    // Other read-only data.
+
+    . = ALIGN(8);
+    .rodata . : { ctor_start_address = .;
+                  *(.ctors) *(.ctors.*)
+                  ctor_end_address = .;
+                  *(rodata*) *(.got2) } > rom
+
+    _RODATA_SECTION_SIZE = . - _RODATA_SECTION_BASE;
+
+    _sbe_image_size = _RODATA_SECTION_SIZE;
+
+    ////////////////////////////////
+    // Read-write Data
+    ////////////////////////////////
+
+    . = SRAM_ORIGIN;
+
+    . = ALIGN(8);
+    _DATA_SECTION_BASE = .;
+
+    // SDA sections .sdata and .sbss must be adjacent to each
+    // other.  Our SDATA sections are small so we'll use strictly positive
+    // offsets.
+
+    _SDA_BASE_ = .;
+    .sdata  . : { *(.sdata*)  } > sram
+    _sbss_start = .;
+    .sbss   . : { *(.sbss*)   } > sram
+    _sbss_end = .;
+
+    // Other read-write data
+    // It's not clear why boot.S is generating empty .glink,.iplt
+
+   .rela   . : { *(.rela*) } > sram
+   .rwdata . : { *(.data*) *(.bss*) } > sram
+
+   //_BASE_LOADER_STACK_LIMIT = .end;
+   //_BASE_LOADER_STACK_LIMIT = . + BASE_LOADER_STACK_SIZE - 1;
+
+    . = ALIGN(8);
+   _PK_INITIAL_STACK_LIMIT = .;
+   . = . + INITIAL_STACK_SIZE;
+   _PK_INITIAL_STACK = . - 1;
+
+    . = ALIGN(8);
+    _loader_end = . - 0;
+}
