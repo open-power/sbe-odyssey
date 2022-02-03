6b592267de532fb3a1cf11712ab0ad4bf09ce402
akhis023 <akhis023@in.ibm.com>
2022-02-03 11:12:14 -0600

    Compile Dilithium Algo
    -Compile Dilithium Algo leveraged from Z
    
    Change-Id: I08f182b39f50d0e88420f98f62c48ffbe869b7db
    Signed-off-by: akhis023 <akhis023@in.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/127216
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>

--- public/src/common/algorithms/pqalgs.h
+++ public/src/common/algorithms/pqalgs.h
@@ -1,7 +1,7 @@
 /* IBM_PROLOG_BEGIN_TAG                                                   */
 /* This is an automatically generated prolog.                             */
 /*                                                                        */
-/* $Source: public/src/common/algorithms/pqalgs.H $                       */
+/* $Source: public/src/common/algorithms/pqalgs.h $                       */
 /*                                                                        */
 /* OpenPOWER sbe Project                                                  */
 /*                                                                        */
