5896eabdcafadcf40f51db158c4a413ca36af259
Thi Tran <thi@us.ibm.com>
2022-03-01 15:10:07 -0500

    P11 - Create level 1 HWPs for isteps 0-3
    These isteps don't have HWPs created yet:
      1.x : Omitted steps that run via command table
      3.25 : Generated HWP
      3.32-3.36, 3.41 : Waiting for interfaces from Chris
      3.50: select boot core
    
    istep 4 HWPs not created since there are already plenty of them in
    the corecache directory. TODO: Close on HWP names with PM team.
    
    Change-Id: Ic864ee6f7b0d3bef6bd6f4bb756ae2ad3ca947f2
    Original-Change-Id: I2cc340fb37b8cd1c13e204454175d4b05c7e26d7
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/128230
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Dev-Ready: Brent Wieman <bwieman@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Joachim Fenkes <fenkes@de.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/128492
    Reviewed-by: Sumit Kumar <sumit_kumar@in.ibm.com>

---
+++ public/src/import/public/hwp/odyssey/perv/odyssey_perv.mk
@@ -0,0 +1,73 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: public/src/import/public/hwp/odyssey/perv/odyssey_perv.mk $
+#
+# OpenPOWER sbe Project
+#
+# Contributors Listed Below - COPYRIGHT 2022
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+#
+# A macro to contain all our boilerplate
+#
+define __ODYSSEY_PERV_PROCEDURE
+PROCEDURE=$(1)
+$$(call ADD_MODULE_INCDIR,$$(PROCEDURE),$(dir $(lastword $(MAKEFILE_LIST)))/../../generic/perv)
+$$(call ADD_MODULE_SRCDIR,$$(PROCEDURE),$(dir $(lastword $(MAKEFILE_LIST)))/../../generic/perv)
+# $$(call ADD_MODULE_OBJ,$$(PROCEDURE),poz_perv_utils.o)
+# $$(call ADD_MODULE_OBJ,$$(PROCEDURE),poz_perv_mod_misc.o)
+# $$(call ADD_MODULE_OBJ,$$(PROCEDURE),poz_perv_mod_chiplet_clocking.o)
+$$(call BUILD_PROCEDURE)
+endef
+ODYSSEY_PERV_PROCEDURE = $(eval $(call __ODYSSEY_PERV_PROCEDURE,$1))
+
+#
+# And now the actual HWP definitions
+#
+
+# istep 0
+$(call ODYSSEY_PERV_PROCEDURE,ody_cbs_start)
+$(call ODYSSEY_PERV_PROCEDURE,ody_sppe_config_update)
+# istep 1
+$(call ODYSSEY_PERV_PROCEDURE,ody_sppe_boot_check)
+$(call ODYSSEY_PERV_PROCEDURE,ody_sppe_attr_setup)
+$(call ODYSSEY_PERV_PROCEDURE,ody_tp_repr_initf)
+$(call ODYSSEY_PERV_PROCEDURE,ody_tp_arrayinit)
+$(call ODYSSEY_PERV_PROCEDURE,ody_tp_arrayinit_cleanup)
+$(call ODYSSEY_PERV_PROCEDURE,ody_tp_initf)
+$(call ODYSSEY_PERV_PROCEDURE,ody_tp_startclocks)
+$(call ODYSSEY_PERV_PROCEDURE,ody_tp_init)
+# istep 2
+
+# istep 3
+$(call ODYSSEY_PERV_PROCEDURE,ody_chiplet_clk_config)
+$(call ODYSSEY_PERV_PROCEDURE,ody_chiplet_reset)
+$(call ODYSSEY_PERV_PROCEDURE,ody_chiplet_unused_psave)
+$(call ODYSSEY_PERV_PROCEDURE,ody_chiplet_pll_setup)
+$(call ODYSSEY_PERV_PROCEDURE,ody_bist_repr_initf)
+$(call ODYSSEY_PERV_PROCEDURE,ody_abist)
+$(call ODYSSEY_PERV_PROCEDURE,ody_lbist)
+$(call ODYSSEY_PERV_PROCEDURE,ody_chiplet_repr_initf)
+$(call ODYSSEY_PERV_PROCEDURE,ody_chiplet_arrayinit)
+$(call ODYSSEY_PERV_PROCEDURE,ody_chiplet_initf)
+$(call ODYSSEY_PERV_PROCEDURE,ody_chiplet_init)
+$(call ODYSSEY_PERV_PROCEDURE,ody_chiplet_startclocks)
+$(call ODYSSEY_PERV_PROCEDURE,ody_chiplet_fir_init)
+$(call ODYSSEY_PERV_PROCEDURE,ody_chiplet_dts_init)
+$(call ODYSSEY_PERV_PROCEDURE,ody_nest_enable_io)
+$(call ODYSSEY_PERV_PROCEDURE,ody_ioppe_load)
