d0af99cb752bf9dbe6c36dcd0a45066869eb0bac
Thi Tran <thi@us.ibm.com>
2022-03-01 15:10:07 -0500

    P11 - Create level 1 HWPs for isteps 0-3
    These isteps don't have HWPs created yet:
      1.x : Omitted steps that run via command table
      3.25 : Generated HWP
      3.32-3.36, 3.41 : Waiting for interfaces from Chris
      3.50: select boot core
    
    istep 4 HWPs not created since there are already plenty of them in
    the corecache directory. TODO: Close on HWP names with PM team.
    
    Change-Id: Ic864ee6f7b0d3bef6bd6f4bb756ae2ad3ca947f2
    Original-Change-Id: I2cc340fb37b8cd1c13e204454175d4b05c7e26d7
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/128230
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Dev-Ready: Brent Wieman <bwieman@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Joachim Fenkes <fenkes@de.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/128492
    Reviewed-by: Sumit Kumar <sumit_kumar@in.ibm.com>

---
+++ public/src/import/public/hwp/odyssey/perv/ody_sppe_boot_check.C
@@ -0,0 +1,49 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: public/src/import/public/hwp/odyssey/perv/ody_sppe_boot_check.C $ */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2022                             */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+//------------------------------------------------------------------------------
+/// @brief
+//------------------------------------------------------------------------------
+// *HWP HW Maintainer   : Anusha Reddy (anusrang@in.ibm.com)
+// *HWP FW Maintainer   : Raja Das (rajadas2@in.ibm.com)
+// *HWP Consumed by     : SPPE, BMC
+//------------------------------------------------------------------------------
+
+#include "ody_sppe_boot_check.H"
+#include "poz_perv_common_params.H"
+
+
+using namespace fapi2;
+
+enum ODY_SPPE_BOOT_CHECK_Private_Constants
+{
+};
+
+ReturnCode ody_sppe_boot_check(const Target<TARGET_TYPE_OCMB_CHIP>& i_target)
+{
+
+
+fapi_try_exit:
+    return current_err;
+}
