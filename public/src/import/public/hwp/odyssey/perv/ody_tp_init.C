fe20329003e084effeae01f1dddd66ea0cc3db71
Daniela Yacovone <falconed@us.ibm.com>
2022-03-28 13:01:47 -0400

    Odyssey istep1 FAPI2 procedures
    Made suggested changes to
    * ody_cbs_start.C
    * ody_tp_arrayinit.C
    * ody_tp_init.C
    
    Change-Id: I106a85e27d7dbd77828b89d3ec2434bbd92fc8f5
    Reviewed-on: https://rchgit01.rchland.ibm.com/gerrit1/#/c/129441/
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/130135
    Reviewed-by: Sumit Kumar <sumit_kumar@in.ibm.com>

--- public/src/import/public/hwp/odyssey/perv/ody_tp_init.C
+++ public/src/import/public/hwp/odyssey/perv/ody_tp_init.C
@@ -25,16 +25,22 @@
 //------------------------------------------------------------------------------
 /// @brief
 //------------------------------------------------------------------------------
-// *HWP HW Maintainer   : Anusha Reddy (anusrang@in.ibm.com)
+// *HWP HW Maintainer   : Daniela Yacovone (falconed@us.ibm.com)
 // *HWP FW Maintainer   : Raja Das (rajadas2@in.ibm.com)
 // *HWP Consumed by     : SPPE
 //------------------------------------------------------------------------------
 
 #include "ody_tp_init.H"
 #include "poz_perv_common_params.H"
+#include "poz_perv_mod_misc.H"
+#include <multicast_defs.H>
+#include <target_filters.H>
+#include <p11_scom_perv.H>
 
+SCOMT_PERV_USE_FSXCOMP_FSXLOG_ROOT_CTRL0;
 
 using namespace fapi2;
+using namespace scomt::perv;
 
 enum ODY_TP_INIT_Private_Constants
 {
@@ -42,8 +48,33 @@ enum ODY_TP_INIT_Private_Constants
 
 ReturnCode ody_tp_init(const Target<TARGET_TYPE_OCMB_CHIP>& i_target)
 {
+    auto l_mc_allgood = i_target.getMulticast<fapi2::TARGET_TYPE_PERV>(fapi2::MCGROUP_GOOD);
+    static const hang_pulse_t SETUP_HANG_COUNTERS_PERV[] = {{0, 16, 0}, {5, 6, 0}, {6, 7, 0, 1}} ;
+    FSXCOMP_FSXLOG_ROOT_CTRL0_t ROOT_CTRL0;
 
+    // TODO : Set up TOD error routing, error mask via scan inits
+    // TODO : Set up perv LFIR, XSTOP_MASK, RECOV_MASK via scan inits
+
+    FAPI_INF("Entering ...");
+    FAPI_DBG("Drop GLOBAL_EP_RESET_DC.");
+    ROOT_CTRL0 = 0;
+    ROOT_CTRL0.set_GLOBAL_EP_RESET_DC(1);
+    FAPI_TRY(ROOT_CTRL0.putScom_CLEAR(i_target));
+
+    FAPI_INF("Start using PCB network");
+    FAPI_TRY(mod_switch_pcbmux(i_target, mux::PCB2PCB));
+
+    FAPI_INF("Set up static multicast groups");
+    FAPI_TRY(mod_multicast_setup(i_target, MCGROUP_GOOD, 0x7FFFFFFFFFFFFFFF, TARGET_STATE_FUNCTIONAL));
+    FAPI_TRY(mod_multicast_setup(i_target, MCGROUP_GOOD_NO_TP, 0x3FFFFFFFFFFFFFFF, TARGET_STATE_FUNCTIONAL));
+
+    FAPI_INF("Set up chiplet hang pulses");
+    FAPI_TRY(mod_hangpulse_setup(l_mc_allgood, 1, SETUP_HANG_COUNTERS_PERV));
+
+    FAPI_INF("Miscellaneous TP setup");
+    FAPI_TRY(mod_poz_tp_init_common(i_target));
 
 fapi_try_exit:
+    FAPI_INF("Exiting ...");
     return current_err;
 }
