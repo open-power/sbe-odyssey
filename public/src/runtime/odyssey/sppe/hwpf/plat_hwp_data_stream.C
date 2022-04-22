5d1f9aa30a7ebc5dc4286a228605f74ff51f7c6e
in0029y2 <anuj.tiwari2@ibm.com>
2022-04-22 01:37:12 -0500

    odyssey specific hwp data stream
    Change-Id: Ida535bcefb993d3458282549b7fdeda98965754f
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/130983
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>

---
+++ public/src/runtime/odyssey/sppe/hwpf/plat_hwp_data_stream.C
@@ -0,0 +1,189 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: public/src/runtime/odyssey/sppe/hwpf/plat_hwp_data_stream.C $ */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2020,2022                        */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+#include "plat_hwp_data_stream.H"
+#include "sbeFifoMsgUtils.H"
+
+using namespace fapi2;
+
+ReturnCodes sbefifo_hwp_data_istream::get(hwp_data_unit& o_data)
+{
+    #define SBE_FUNC "sbefifo_hwp_data_istream::get"
+    SBE_ENTER(SBE_FUNC);
+    if (iv_words_left == 0)
+    {
+        return FAPI2_RC_FALSE;
+    }
+    uint32_t len = 1;
+    uint32_t rc = SBE_SEC_OPERATION_SUCCESSFUL;
+    if(iv_doFifoAccess)
+    {   // PopUp data from the upstream FIFO
+        rc = sbeUpFifoDeq_mult(len, &o_data, iv_words_left == 1,
+                               false, iv_fifoType);
+        if (rc)
+        {
+            return FAPI2_RC_PLAT_ERR_SEE_DATA;
+        }
+    }
+    iv_words_left--;
+    return FAPI2_RC_SUCCESS;
+    #undef SBE_FUNC
+}
+
+uint32_t sbefifo_hwp_data_istream::get( uint32_t o_length, uint32_t* o_buffer,
+                                        bool i_isEot, bool i_flush )
+{
+    #define SBE_FUNC "sbefifo_hwp_data_istream::get with length"
+    SBE_ENTER(SBE_FUNC);
+    uint32_t rc = SBE_SEC_OPERATION_SUCCESSFUL;
+    uint32_t len = o_length;
+
+    if(iv_doFifoAccess)
+    {   // Push data into the downstream FIFO
+        rc = sbeUpFifoDeq_mult(len, o_buffer, i_isEot,
+                               i_flush, iv_fifoType);
+    }
+    else
+    {
+        if(iv_data == NULL)
+        {
+            rc = SBE_SEC_GET_DUMP_STREAM_FAILED;
+        }
+        for( uint8_t i=0; i< o_length; i++ )
+            o_buffer[i] = iv_data[i];
+    }
+    iv_words_left += o_length;
+    return rc;
+    #undef SBE_FUNC
+}
+
+ReturnCodes sbefifo_hwp_data_ostream::put(hwp_data_unit i_data)
+{
+#define SBE_FUNC "sbefifo_hwp_data_ostream::put(): 1 word"
+    SBE_ENTER(SBE_FUNC);
+    uint32_t len = 1; //1 word ie 4 bytes
+    uint32_t rc = SBE_SEC_OPERATION_SUCCESSFUL;
+    if(iv_hbMemAddr) //Dump the data into the memory
+    {
+        //ReturnCode rc  = iv_memInterface.accessWithBuffer(
+        //                                      &i_data, 4, iv_isPBALastAccess);
+        if(rc)
+        {
+            SBE_ERROR(SBE_FUNC " PBA write failed in accessWithBuffer");
+            return FAPI2_RC_PLAT_ERR_SEE_DATA;
+        }
+    }
+    else
+    {
+        rc = sbeDownFifoEnq_mult(len, &i_data, iv_fifoType);
+        if (rc)
+        {
+            SBE_ERROR(SBE_FUNC " sbeDownFifoEnq_mult failed");
+            return FAPI2_RC_PLAT_ERR_SEE_DATA;
+        }
+    }
+    iv_words_written++;
+    return FAPI2_RC_SUCCESS;
+#undef SBE_FUNC
+}
+
+uint32_t sbefifo_hwp_data_ostream::put(uint32_t i_length, uint32_t* i_buffer)
+{
+#define SBE_FUNC "sbefifo_hwp_data_ostream::put()"
+    SBE_ENTER(SBE_FUNC);
+    uint32_t rc = SBE_SEC_OPERATION_SUCCESSFUL;
+    ReturnCode fapiRc = FAPI2_RC_SUCCESS;
+    if(iv_hbMemAddr) //Dump the data into the memory
+    {
+        //fapiRc  = iv_memInterface.accessWithBuffer(
+        //                          i_buffer, (i_length*4), iv_isPBALastAccess);
+        if(fapiRc)
+        {
+            SBE_ERROR(SBE_FUNC " PBA write failed in accessWithBuffer");
+            return SBE_SEC_GET_DUMP_STREAM_FAILED;
+        }
+    }
+    else
+    {
+        // Push data into the downstream FIFO
+        rc = sbeDownFifoEnq_mult (i_length, i_buffer, iv_fifoType);
+        if (rc)
+        {
+            SBE_ERROR(SBE_FUNC " sbeDownFifoEnq_mult failed");
+            return rc;
+        }
+    }
+    iv_words_written = iv_words_written + i_length;
+    return rc;
+#undef SBE_FUNC
+}
+
+seeprom_hwp_data_istream::seeprom_hwp_data_istream(const uint32_t *i_data, size_t i_size) :
+    // round iv_data up to an 8byte boundary
+    iv_data(reinterpret_cast<uint64_t *>(reinterpret_cast<unsigned long>(i_data + 1) & ~7)),
+    // determine whether i_data starts on an even or odd 4-byte word
+    iv_odd(reinterpret_cast<unsigned long>(i_data) & 4),
+    // round size up to a multiple of 4 bytes
+    iv_words_left((i_size + 3) >> 2)
+{
+    if (iv_odd)
+    {
+        // i_data begins on an odd word boundary; we already
+        // set iv_data to point to the next even word, and now
+        // have to read the first word we skipped into iv_2ndword
+        // for the first get() operation.
+        iv_2ndword = *i_data;
+    }
+}
+
+ReturnCodes seeprom_hwp_data_istream::get(hwp_data_unit& o_data)
+{
+    #define SBE_FUNC "seeprom_hwp_data_ostream::get"
+    SBE_ENTER(SBE_FUNC);
+    if (iv_words_left == 0)
+    {
+        return FAPI2_RC_FALSE;
+    }
+    else if (iv_odd)
+    {
+        // Return the previously stored lower half
+        o_data = iv_2ndword;
+    }
+    else
+    {
+        // Read one 8-byte word from the SEEPROM
+        uint64_t seeprom_data;
+        PPE_LVD(iv_data, seeprom_data);
+        iv_data++;
+
+        // Return the upper half, store the lower half
+        o_data = seeprom_data >> 32;
+        iv_2ndword = seeprom_data & 0xFFFFFFFF;
+    }
+
+    iv_words_left--;
+    iv_odd = !iv_odd;
+    return FAPI2_RC_SUCCESS;
+    #undef SBE_FUNC
+}
