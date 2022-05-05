def65e85ac74d10a7eb22f2579430476406be66c
Joachim Fenkes <fenkes@de.ibm.com>
2022-05-05 18:39:10 +0200

    FAPI2: Add new APIs for scan infrastructure
    Add APIs to access files in an embedded archive,
    and an backend API for ring scanning. Also add a simple
    platform implementation and unit test.
    
    Change-Id: Ia53276cc486bbef630d37f68a6355538678c8821
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/131334
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Kahn C Evans <kahnevan@us.ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Reviewed-by: Prem Shanker Jha <premjha2@in.ibm.com>
    Reviewed-by: Joachim Fenkes <fenkes@de.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/131336
    Reviewed-by: Team <hostboot@us.ibm.com>
    Reviewed-by: Sumit Kumar <sumit_kumar@in.ibm.com>

---
+++ public/src/import/public/common/generic/fapi2/test/fapi2_file_test.C
@@ -0,0 +1,43 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: public/src/import/public/common/generic/fapi2/test/fapi2_file_test.C $ */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2022                             */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+#define FAPI2_NO_FFDC
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <file_access.H>
+
+using namespace fapi2;
+
+TEST_CASE("files")
+{
+    void* data;
+    size_t size;
+    const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP> l_target;
+
+    ReturnCode rc = loadEmbeddedFile(l_target, "fapi2_file_test.C", data, size);
+    REQUIRE( rc == FAPI2_RC_SUCCESS );
+
+    rc = freeEmbeddedFile(data);
+    REQUIRE( rc == FAPI2_RC_SUCCESS );
+}
