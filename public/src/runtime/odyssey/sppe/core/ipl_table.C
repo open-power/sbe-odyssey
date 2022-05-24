b8f266ae7636034edb076df7c98ccd4b84253400
rajerpp1 <rajerpp1@in.ibm.com>
2022-05-24 07:09:39 -0500

    Enabling istep 1 (1.18 to 1.22)
    Change-Id: Id519f58462a37c2795e938b0a924f4d546c887c1
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/132291
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>

--- public/src/runtime/odyssey/sppe/core/ipl_table.C
+++ public/src/runtime/odyssey/sppe/core/ipl_table.C
@@ -25,15 +25,35 @@
 #include "ipl.H"
 #include "return_code.H"
 #include "sbetrace.H"
+#include "sbeutil.H"
+#include "target.H"
+#include <ody_tp_arrayinit.H>
+#include <ody_tp_arrayinit_cleanup.H>
+#include <ody_tp_initf.H>
+#include <ody_tp_startclocks.H>
+#include <ody_tp_init.H>
 
 using namespace fapi2;
 
 //----------------------------------------------------------------------------
 
+using  sbeIstepHwpOcmb_t = ReturnCode (*)
+                      (const Target<TARGET_TYPE_OCMB_CHIP> & i_target);
+
 ReturnCode istepNoOp( voidfuncptr_t i_hwp)
 {
     SBE_INFO("istepNoOp");
     return FAPI2_RC_SUCCESS ;
+}
+
+ReturnCode istepWithOcmb( voidfuncptr_t i_hwp)
+{
+    SBE_INFO("[DBG] : istepWithOcmb : starting");
+    ReturnCode rc = FAPI2_RC_SUCCESS;
+    Target<TARGET_TYPE_OCMB_CHIP > l_ocmb_chip = g_platTarget->plat_getChipTarget();
+    assert( NULL != i_hwp );
+    SBE_EXEC_HWP(rc, reinterpret_cast<sbeIstepHwpOcmb_t>( i_hwp ), l_ocmb_chip);
+    return rc;
 }
 
 static istepMap_t g_istep1PtrTbl[] =
@@ -55,11 +75,11 @@ static istepMap_t g_istep1PtrTbl[] =
              ISTEP_MAP( NULL, NULL ),
              ISTEP_MAP( NULL, NULL ),
              ISTEP_MAP( NULL, NULL ),
-             ISTEP_MAP( NULL, NULL ),
-             ISTEP_MAP( NULL, NULL ),
-             ISTEP_MAP( NULL, NULL ),
-             ISTEP_MAP( NULL, NULL ),
-             ISTEP_MAP( NULL, NULL ),
+             ISTEP_MAP( istepWithOcmb, ody_tp_arrayinit ),
+             ISTEP_MAP( istepWithOcmb, ody_tp_arrayinit_cleanup ),
+             ISTEP_MAP( istepWithOcmb, ody_tp_initf ),
+             ISTEP_MAP( istepWithOcmb, ody_tp_startclocks ),
+             ISTEP_MAP( istepWithOcmb, ody_tp_init ),
          };
 
 istepTableEntry_t istepTableEntries[] = {
