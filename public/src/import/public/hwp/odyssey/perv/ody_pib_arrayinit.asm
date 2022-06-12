8bc7984ae9468e92ece46c6d5a585325d7e96205
Sreekanth Reddy Kadapala <skadapal@in.ibm.com>
2022-06-12 07:08:09 -0400

    P11 SPINAL - istep s1 assembly procedures
    * resolved merge conflict
    * addressed review comments
    * updated odyssey code as well
    * extracted common code as commented by Joachim
    * scan_types updated for odyssey
    * added p11s_pib_startclocks procedure
    * Break out common assembly into shared modules
    * Clean up some register definitions
    
    Change-Id: I0f1ed46799e94568a560480b5a29d9cfc7f58bf6
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/132751
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Joachim Fenkes <fenkes@de.ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/132753
    Reviewed-by: Team <hostboot@us.ibm.com>
    Reviewed-by: Sumit Kumar <sumit_kumar@in.ibm.com>

--- public/src/import/public/hwp/odyssey/perv/ody_pib_arrayinit.asm
+++ public/src/import/public/hwp/odyssey/perv/ody_pib_arrayinit.asm
@@ -23,7 +23,7 @@
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
 //------------------------------------------------------------------------------
-/// @brief
+/// @brief Array Init for PIB clock region
 //------------------------------------------------------------------------------
 // *HWP HW Maintainer   : Daniela Yacovone (falconed@us.ibm.com)
 // *HWP FW Maintainer   : Raja Das (rajadas2@in.ibm.com)
@@ -36,42 +36,6 @@
 #include "cmdtable_hwp_errors.H"
 
 ody_pib_arrayinit:
-
-mod_abist_start:
-         // Switch dual-clocked arrays to ABIST clock domain
-         putscom     CPLT_CTRL0_WO_OR, ALL, CPLT_CTRL0__CTRL_CC_ABSTCLK_MUXSEL_DC         //CPLT_CTRL0.set_CTRL_CC_ABSTCLK_MUXSEL_DC(1);
-
-         // Set up BISTed regions
-         //BIST = 0
-         //BIST.set_TC_SRAM_ABIST_MODE_DC(1);
-         //BIST[16 bits starting at BIST_REGION_PERV] = i_clock_regions    //perv regions: 0-3, 10
-         putscom     BIST, ALL, BIST__TC_SRAM_ABIST_MODE_DC | BIST__REGIONS(I_CLOCK_REGIONS)
-
-         //Set up clocking
-         //CLK_REGION = 0
-         //CLK_REGION[16 bits starting at CLOCK_REGION_PERV] = i_clock_regions    //perv regions: 0-3, 10
-         //CLK_REGION.set_SEL_THOLD_SL(1);
-         //CLK_REGION.set_SEL_THOLD_NSL(1);
-         //CLK_REGION.set_SEL_THOLD_ARY(1);
-         putscom      CLK_REGION, ALL, CLK_REGION__REGIONS(I_CLOCK_REGIONS) | CLK_REGION__SEL_THOLD_SL | CLK_REGION__SEL_THOLD_NSL | CLK_REGION__SEL_THOLD_ARY
-
-         // Configure idle count
-         //OPCG_REG1[bits 0:35] = i_abist_start_at
-         putscom       OPCG_REG1, 0xlFFFFFFFFF, PERV_ABIST_START_AT
-
-         // Configure loop count and start OPCG
-         //OPCG_REG0 = 0
-         //OPCG_REG0.set_RUNN_MODE(1);
-         //OPCG_REG0.set_OPCG_GO(1);
-         //OPCG_REG0.set_OPCG_STARTS_BIST(1);
-         //OPCG_REG0.set_LOOP_COUNT(i_runn_cycles)
-         putscom       OPCG_REG0, ALL, OPCG_REG0__RUNN_MODE | OPCG_REG0__OPCG_GO | OPCG_REG0__OPCG_START_BIST | OPCG_REG0__LOOP_COUNT(I_RUNN_CYCLES)
-
-mod_abist_poll:
-         //FAPI_TRY(poll_opcg_done(i_target, 200us, 1120kcyc, 400));
-         poll     CPLT_STAT, OPCG_DONE, OPCG_DONE, ERR_OPCG_TIMEOUT_ABIST
-
-         // Check that ABIST_DONE is set
-         //if not CPLT_STAT0.ABIST_DONE:
-           //FAPI_TRY(ASSERT(SRAM_ABIST_DONE_BIT_ERR));
-         poll     CPLT_STAT, ABIST_DONE, ABIST_DONE, ERR_ABIST_TIMEOUT
+        // Call the poz_pib_arrayinit module with correct parameters
+        #define I_CLOCK_REGIONS PIB_CLOCK_REGION
+        #include "poz_pib_arrayinit.asm"
