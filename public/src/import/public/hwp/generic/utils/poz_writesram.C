2455715b6e55151283807f2d0ee1426183cb4fb6
Thi Tran <thi@us.ibm.com>
2022-07-18 13:51:38 -0400

    Add ody_omi_phy_ppe_load HWP
    Change-Id: If585bea4b6b20f6700885501321be99263ec8e08
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/134309
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Chris Steffen <cwsteffen@us.ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/134311
    Reviewed-by: Sumit Kumar <sumit_kumar@in.ibm.com>

---
+++ public/src/import/public/hwp/generic/utils/poz_writesram.C
@@ -0,0 +1,102 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: public/src/import/public/hwp/generic/utils/poz_writesram.C $  */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2022                             */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file poz_writesram.C
+/// @brief Write data to chip SRAM
+/// *HWP HW Maintainer: Thi Tran <thi@us.ibm.com>
+/// *HWP FW Maintainer:
+/// *HWP Consumed by: HB, Cronus, SBE
+///
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+#include <poz_writesram.H>
+
+//------------------------------------------------------------------------------
+// Function definitions
+//------------------------------------------------------------------------------
+
+/////////////////////////////////////////////////
+/// poz_writesram
+/////////////////////////////////////////////////
+fapi2::ReturnCode poz_writesram(const fapi2::Target <
+                                fapi2::TARGET_TYPE_OCMB_CHIP |
+                                fapi2::TARGET_TYPE_COMPUTE_CHIP |
+                                fapi2::TARGET_TYPE_TBUSC |
+                                fapi2::TARGET_TYPE_IOHS |
+                                fapi2::TARGET_TYPE_MC |
+                                fapi2::TARGET_TYPE_MULTICAST > & i_target,
+                                const uint32_t i_controlReg,
+                                const uint32_t i_dataReg,
+                                const uint32_t i_bytes,
+                                uint8_t* i_data)
+{
+    FAPI_DBG("Start");
+    fapi2::buffer<uint64_t> l_data64 = 0;
+    uint8_t* l_dataPtr = i_data;
+
+    // Enable auto increment
+    if (i_bytes > 8)
+    {
+        FAPI_DBG("Enable auto-increment.");
+        l_data64.setBit<0>();
+        FAPI_TRY(fapi2::putScom(i_target, i_controlReg, l_data64));
+    }
+
+    // Write data
+    while (l_dataPtr < (i_data + i_bytes))
+    {
+        l_data64 = 0;
+
+        // Load 8 bytes into 64-bit word
+        for (uint8_t ii = 0; ii < 8; ii++)
+        {
+            l_data64 |= ( static_cast<uint64_t>(*l_dataPtr++) << (56 - (8 * ii)) );
+
+            // Exit if size has reached. Remaining data in double words are zeroes
+            if (l_dataPtr >= i_data + i_bytes)
+            {
+                break;
+            }
+        }
+
+        FAPI_DBG("Write data: l_dataPtr %p, l_data64 = 0x%.16llX", l_dataPtr - 1, l_data64);
+        FAPI_TRY(fapi2::putScom(i_target, i_dataReg, l_data64),
+                 "Error from putScom");
+    }
+
+    // Disable auto-increment
+    if (i_bytes > 8)
+    {
+        FAPI_DBG("Disable auto-increment.");
+        l_data64.clearBit<0>();
+        FAPI_TRY(fapi2::putScom(i_target, i_controlReg, l_data64));
+    }
+
+fapi_try_exit:
+    FAPI_DBG("End");
+    return fapi2::current_err;
+}
---
+++ public/src/import/public/hwp/odyssey/io/ody_putsram.C
@@ -0,0 +1,75 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: public/src/import/public/hwp/odyssey/io/ody_putsram.C $       */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2022                             */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file ody_putsram.C
+/// @brief Write data to SRAM
+/// *HWP HW Maintainer: Thi Tran <thi@us.ibm.com>
+/// *HWP FW Maintainer:
+/// *HWP Consumed by: HB, Cronus, SBE
+///
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+#include <ody_putsram.H>
+#include <poz_writesram.H>
+#include <ody_scom_omi_ioo.H>
+
+//------------------------------------------------------------------------------
+// Constants
+//------------------------------------------------------------------------------
+// OCMB
+SCOMT_OMI_USE_PHY_PPE_WRAP0_ARB_CSAR
+
+//------------------------------------------------------------------------------
+// Function definitions
+//------------------------------------------------------------------------------
+fapi2::ReturnCode ody_putsram(const fapi2::Target <fapi2::TARGET_TYPE_OCMB_CHIP>& i_target,
+                              const uint64_t i_offset,
+                              const uint32_t i_bytes,
+                              uint8_t* i_data)
+{
+    FAPI_DBG("Start");
+    using namespace scomt::omi;
+    PHY_PPE_WRAP0_ARB_CSAR_t  WRAP0_ARB_CSAR;
+
+    FAPI_DBG("i_offset [0x%.8X%.8X], i_bytes %u.",
+             ((i_offset >> 32) & 0xFFFFFFFF), (i_offset & 0xFFFFFFFF), i_bytes);
+
+    // Set SRAM address
+    WRAP0_ARB_CSAR = i_offset;
+    FAPI_TRY(WRAP0_ARB_CSAR.putScom(i_target),
+             "Error putscom to WRAP0_ARB_CSAR (SRAM address).");
+
+    // Write SRAM
+    FAPI_TRY(poz_writesram(i_target, PHY_PPE_WRAP0_ARB_CSCR_RW, PHY_PPE_WRAP0_ARB_CSDR, i_bytes, i_data),
+             "Error from poz_writesram (Odyssey).");
+
+    FAPI_DBG("poz_writesram completes.");
+
+fapi_try_exit:
+    FAPI_DBG("End");
+    return fapi2::current_err;
+}
