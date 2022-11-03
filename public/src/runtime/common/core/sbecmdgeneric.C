01089603bc09940c6f0adc9fbe83a3309f54357b
Sumit Kumar <sumit_kumar@in.ibm.com>
2022-11-03 12:09:47 -0500

    Code update: Base chip-op framework
    This commit is to get the base chip-op framework
    for code-update in place. The intention is to get
    command-class and op-code for all chip-ops defined
    in a source file and each called chip-op can then
    be defined in separate files stacked on top of this
    commit.
    
    Also all the common functions that may be needed
    across different code-update chip-ops can be defined
    here.
    
    - Added getCodeLevels chip-op (class AE, opcode 01)
    - Added updateImage chip-op   (class AE, opcode 02)
    - Added syncPartition chip-op (class AE, opcode 03)
    - Added getCapability chip-op (class A8, opcode 02)
    
    Jira Id: PFSBE-5
    
    Change-Id: Iaa9b98e17c01ba6f907536a1a603232c2670d0c8
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/140856
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>
    Reviewed-by: Ramesh I <rameshi1@in.ibm.com>
    Reviewed-by: AMIT J TENDOLKAR <amit.tendolkar@in.ibm.com>

---
+++ public/src/runtime/common/core/sbecmdgeneric.C
@@ -0,0 +1,88 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: public/src/runtime/common/core/sbecmdgeneric.C $              */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2016,2023                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+#include "sbecodeupdate.H"
+#include "sbecmdgeneric.H"
+#include "cmnglobals.H"
+#include "ppe42_string.h"
+#include "sbetrace.H"
+#include "p11_scom_perv_cfam.H"
+#include "p11_ppe_pc.H"
+#include "mbxscratch.H"
+#include "errorcodes.H"
+#include "sbefifo.H"
+#include "chipop_struct.H"
+#include "sbeFifoMsgUtils.H"
+
+
+////////////////////////////////////////////////////////
+//////// Get Capability Chip-op ////////////////////////
+////////////////////////////////////////////////////////
+uint32_t sbeGetCapability (uint8_t *i_pArg)
+{
+#define SBE_FUNC " sbeGetCapability "
+    SBE_ENTER(SBE_FUNC);
+
+    uint32_t l_rc = SBE_SEC_OPERATION_SUCCESSFUL;
+    //TODO:PFSBE-6 to be uncommented during implementation
+    //uint32_t fapiRc = fapi2::FAPI2_RC_SUCCESS;
+    //getCapabilityRespMsg_t msg;
+    sbeRespGenHdr_t hdr;
+    hdr.init();
+    sbeResponseFfdc_t ffdc;
+    sbeFifoType type;
+
+    do
+    {
+        chipOpParam_t* configStr = (struct chipOpParam*)i_pArg;
+        type = static_cast<sbeFifoType>(configStr->fifoType);
+        SBE_DEBUG(SBE_FUNC "Fifo Type is:[%02X]",type);
+
+#if 0   //TODO: to be enabled during implementation
+        //No attempt to read FIFO as no input params expected
+        // call get capability function
+        fapiRc = getCapability(&msg, type);
+        if (fapiRc != fapi2::FAPI2_RC_SUCCESS)
+        {
+            l_rc = CU_RC_GET_CAPABILITY_HWP_FAILURE;
+            hdr.setStatus( SBE_PRI_GENERIC_EXECUTION_FAILURE,
+                           l_rc );
+            ffdc.setRc(l_rc);
+            break;
+        }
+#endif
+    } while(false);
+
+    // Build the response header packet
+    l_rc = sbeDsSendRespHdr(hdr, &ffdc, type);
+    if(l_rc != SBE_SEC_OPERATION_SUCCESSFUL)
+    {
+        SBE_ERROR(SBE_FUNC"Failed. rc[0x%X]",l_rc);
+    }
+
+    SBE_EXIT(SBE_FUNC);
+    return l_rc;
+#undef SBE_FUNC
+}
+
---
+++ public/src/runtime/common/core/sbecodeupdate.C
@@ -0,0 +1,202 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: public/src/runtime/common/core/sbecodeupdate.C $              */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2016,2023                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+#include "sbecodeupdate.H"
+#include "cmnglobals.H"
+#include "ppe42_string.h"
+#include "sbetrace.H"
+#include "p11_scom_perv_cfam.H"
+#include "p11_ppe_pc.H"
+#include "mbxscratch.H"
+#include "errorcodes.H"
+#include "sbefifo.H"
+#include "chipop_struct.H"
+#include "sbeFifoMsgUtils.H"
+
+////////////////////////////////////////////////////////
+//////// Get Code Levels Chip-op ///////////////////////
+////////////////////////////////////////////////////////
+uint32_t sbeGetCodeLevels (uint8_t *i_pArg)
+{
+#define SBE_FUNC " sbeGetCodeLevels "
+    SBE_ENTER(SBE_FUNC);
+
+    uint32_t l_rc = SBE_SEC_OPERATION_SUCCESSFUL;
+    //TODO:PFSBE163 to be uncommented during implementation
+    //uint32_t fapiRc = fapi2::FAPI2_RC_SUCCESS;
+    //getCodeLevelsRespMsg_t msg;
+    sbeRespGenHdr_t hdr;
+    hdr.init();
+    sbeResponseFfdc_t ffdc;
+    sbeFifoType type;
+
+    do
+    {
+        chipOpParam_t* configStr = (struct chipOpParam*)i_pArg;
+        type = static_cast<sbeFifoType>(configStr->fifoType);
+        SBE_DEBUG(SBE_FUNC "Fifo Type is:[%02X]",type);
+
+#if 0   //TODO: to be enabled during implementation
+        //No attempt to read FIFO as no input params expected
+        // call get code levels function
+        fapiRc = getCodeLevels(&msg, type);
+        if (fapiRc != fapi2::FAPI2_RC_SUCCESS)
+        {
+            l_rc = CU_RC_CODE_LEVELS_HWP_FAILURE;
+            hdr.setStatus( SBE_PRI_GENERIC_EXECUTION_FAILURE,
+                           l_rc );
+            ffdc.setRc(l_rc);
+            break;
+        }
+#endif
+    } while(false);
+
+    // Build the response header packet
+    l_rc = sbeDsSendRespHdr(hdr, &ffdc, type);
+    if(l_rc != SBE_SEC_OPERATION_SUCCESSFUL)
+    {
+        SBE_ERROR(SBE_FUNC"Failed. rc[0x%X]",l_rc);
+    }
+
+    SBE_EXIT(SBE_FUNC);
+    return l_rc;
+#undef SBE_FUNC
+}
+
+////////////////////////////////////////////////////////
+//////// Update Image Chip-op //////////////////////////
+////////////////////////////////////////////////////////
+uint32_t sbeUpdateImage (uint8_t *i_pArg)
+{
+#define SBE_FUNC " sbeUpdateImage "
+    SBE_ENTER(SBE_FUNC);
+
+    uint32_t l_rc = SBE_SEC_OPERATION_SUCCESSFUL;
+    //TODO:PFSBE-20 to be uncommented during implementation
+    //uint32_t fapiRc = fapi2::FAPI2_RC_SUCCESS;
+    //getUpdateImageRespMsg_t msg;
+    sbeRespGenHdr_t hdr;
+    hdr.init();
+    sbeResponseFfdc_t ffdc;
+    sbeFifoType type;
+
+    do
+    {
+        chipOpParam_t* configStr = (struct chipOpParam*)i_pArg;
+        type = static_cast<sbeFifoType>(configStr->fifoType);
+        SBE_DEBUG(SBE_FUNC "Fifo Type is:[%02X]",type);
+
+#if 0   //TODO: to be enabled during implementation
+        // Will attempt to dequeue three entries for the update image
+        // plus the expected EOT entry at the end
+        uint32_t len2dequeue = sizeof(msg)/sizeof(uint32_t);
+        l_rc = sbeUpFifoDeq_mult (len2dequeue, (uint32_t *)&msg, false, false, type);
+        if(l_rc != SBE_SEC_OPERATION_SUCCESSFUL)
+        {
+            break;
+        }
+
+        // call update image function
+        fapiRc = updateImage(&msg, type);
+        if (fapiRc != fapi2::FAPI2_RC_SUCCESS)
+        {
+            l_rc = CU_RC_UPDATE_IMAGE_HWP_FAILURE;
+            hdr.setStatus( SBE_PRI_GENERIC_EXECUTION_FAILURE,
+                           l_rc );
+            ffdc.setRc(l_rc);
+        }
+#endif
+
+        // Build the response header packet
+        l_rc = sbeDsSendRespHdr(hdr, &ffdc, type);
+    } while(false);
+
+    if(l_rc != SBE_SEC_OPERATION_SUCCESSFUL)
+    {
+        SBE_ERROR(SBE_FUNC"Failed. rc[0x%X]",l_rc);
+    }
+
+    SBE_EXIT(SBE_FUNC);
+    return l_rc;
+#undef SBE_FUNC
+}
+
+////////////////////////////////////////////////////////
+//////// Sync Partition Chip-op ////////////////////////
+////////////////////////////////////////////////////////
+uint32_t sbeSyncPartition (uint8_t *i_pArg)
+{
+#define SBE_FUNC " sbeSyncPartition "
+    SBE_ENTER(SBE_FUNC);
+
+    uint32_t l_rc = SBE_SEC_OPERATION_SUCCESSFUL;
+    //TODO:PFSBE-21 to be uncommented during implementation
+    //uint32_t fapiRc = fapi2::FAPI2_RC_SUCCESS;
+    //syncPartitionRespMsg_t msg;
+    sbeRespGenHdr_t hdr;
+    hdr.init();
+    sbeResponseFfdc_t ffdc;
+    sbeFifoType type;
+
+    do
+    {
+        chipOpParam_t* configStr = (struct chipOpParam*)i_pArg;
+        type = static_cast<sbeFifoType>(configStr->fifoType);
+        SBE_DEBUG(SBE_FUNC "Fifo Type is:[%02X]",type);
+
+#if 0   //TODO: to be enabled during implementation
+        //No attempt to read FIFO as no input params expected
+        // call sync partition function
+        fapiRc = syncPartition(&msg, type);
+        if (fapiRc != fapi2::FAPI2_RC_SUCCESS)
+        {
+            l_rc = CU_RC_SYNC_PARTITION_HWP_FAILURE;
+            hdr.setStatus( SBE_PRI_GENERIC_EXECUTION_FAILURE,
+                           l_rc );
+            ffdc.setRc(l_rc);
+            break;
+        }
+#endif
+    } while(false);
+
+    // Build the response header packet
+    l_rc = sbeDsSendRespHdr(hdr, &ffdc, type);
+    if(l_rc != SBE_SEC_OPERATION_SUCCESSFUL)
+    {
+        SBE_ERROR(SBE_FUNC"Failed. rc[0x%X]",l_rc);
+    }
+
+    SBE_EXIT(SBE_FUNC);
+    return l_rc;
+#undef SBE_FUNC
+}
+
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+
+uint8_t getBootedPartition(void)
+{
+    return PARTITION_0_INDEX;
+}
+
