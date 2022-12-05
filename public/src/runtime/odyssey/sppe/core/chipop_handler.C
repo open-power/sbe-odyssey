7d357becab5c9cff5b9e553209d15688e6c5066c
akhis023 <akhis023@in.ibm.com>
2022-12-05 06:09:59 -0600

    Common code refactor: Chip-op handler
    JIRA ID: PFSBE-126
    
    Change-Id: Ia74aa8ec2468327a2cf542485c1ff21e297847d7
    Signed-off-by: akhis023 <akhis023@in.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/142078
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>
    Reviewed-by: Anuj Tiwari <anuj.tiwari2@ibm.com>
    Reviewed-by: Rangaswamy H B . <rangaswamy.h.b@ibm.com>
    Reviewed-by: AMIT J TENDOLKAR <amit.tendolkar@in.ibm.com>

--- public/src/runtime/odyssey/sppe/core/chipop_handler.C
+++ public/src/runtime/odyssey/sppe/core/chipop_handler.C
@@ -30,93 +30,9 @@
 #include "cmd_class_init.H"
 #include "chipop_handler.H"
 #include "fences.H"
-////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////
-uint8_t sbeGetCmdStructAttr (const uint8_t  i_cmdClass,
-                                   cmdStruct_t **o_ppCmd)
-{
-    #define SBE_FUNC " sbeGetCmdStructAttr "
-    uint8_t l_numCmds  = 0;
-    *o_ppCmd = NULL;
-
-    size_t cmdClassIndex = HASH_KEY(i_cmdClass);
-    if(cmdClassIndex < cmdClassTable.len &&
-       cmdClassTable.cmdClassArr[cmdClassIndex].cmdTable)
-    {
-        l_numCmds = cmdClassTable.cmdClassArr[cmdClassIndex].cmdTable->len;
-        *o_ppCmd = cmdClassTable.cmdClassArr[cmdClassIndex].cmdTable->cmdArr;
-    }
-
-    return l_numCmds;
-    #undef SBE_FUNC
-}
 
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
-uint8_t sbeValidateCmdClass (const uint8_t i_cmdClass,
-                             const uint8_t i_cmdOpcode)
-{
-    #define SBE_FUNC " sbeValidateCmdClass "
-    uint8_t l_rc           = SBE_SEC_COMMAND_NOT_SUPPORTED;
-
-    SBE_INFO(SBE_FUNC"i_cmdClass[0x%02X], "
-              "i_cmdOpcode[0x%02X]", i_cmdClass, i_cmdOpcode);
-
-    do
-    {
-        uint8_t l_numCmds      = 0;
-        cmdStruct_t *l_pCmd = NULL;
-
-        l_numCmds = sbeGetCmdStructAttr (i_cmdClass, &l_pCmd);
-        if (!l_numCmds)
-        {
-            SBE_ERROR(SBE_FUNC"SBE_SEC_COMMAND_CLASS_NOT_SUPPORTED "
-                "i_cmdClass[0x%02X], i_cmdOpcode[0x%02X]",
-                i_cmdClass, i_cmdOpcode);
-            // Command class not supported
-            l_rc = SBE_SEC_COMMAND_CLASS_NOT_SUPPORTED;
-            break;
-        }
-
-        // @TODO via RTC : 128654
-        //       Analyze on merging the validation functions into one
-        //       and also on using loop vs switch case performance
-        for (uint8_t l_cnt = 0; l_cnt < l_numCmds; ++l_cnt, ++l_pCmd)
-        {
-            if (i_cmdOpcode == l_pCmd->cmd_opcode)
-            {
-                // Command found
-                l_rc = SBE_SEC_OPERATION_SUCCESSFUL;
-                break;
-            }
-        }
-    } while (false);
-
-    return l_rc;
-    #undef SBE_FUNC
-}
-
-sbeCmdFunc_t sbeFindCmdFunc (const uint8_t i_cmdClass,
-                             const uint8_t i_cmdOpcode)
-
-{
-    #define SBE_FUNC " sbeFindCmdFunc "
-    uint8_t l_numCmds      = 0;
-    cmdStruct_t *l_pCmd = NULL;
-
-    l_numCmds = sbeGetCmdStructAttr (i_cmdClass, &l_pCmd);
-
-    for (uint8_t l_cnt = 0; l_cnt < l_numCmds; ++l_cnt, ++l_pCmd)
-    {
-        if (i_cmdOpcode == l_pCmd->cmd_opcode)
-        {
-            break;
-        }
-    }
-
-    return l_pCmd ? (l_pCmd->cmd_func) : NULL;
-    #undef SBE_FUNC
-}
 
 sbeChipOpRc_t sbeIsCmdAllowed (const uint8_t i_cmdClass,
                                const uint8_t i_cmdOpcode)
