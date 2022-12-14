4ccaadc7e8339a6bd64182521b2b8dc78f6b5a3b
Olsen <cmolsen@us.ibm.com>
2022-12-14 15:57:06 -0500

    Updates associated with OCC memory layout updates
    Key_Build=SPLIT_DIR_CHECK_IGNORE
    
    Change-Id: I478fa95fac6b763a052460908d6e2830eafc4533
    Original-Change-Id: If896e618c2a714356228ed125205eda745e80f18
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/142644
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: DAVID DU <daviddu@us.ibm.com>
    Reviewed-by: Greg Still <stillgs@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/149142
    Reviewed-by: Sumit Kumar <sumit_kumar@in.ibm.com>

--- public/src/import/public/emb/p11/kernels/ppe/powmanlib/pmbuild_common.mk
+++ public/src/import/public/emb/p11/kernels/ppe/powmanlib/pmbuild_common.mk
@@ -5,7 +5,8 @@
 #
 # OpenPOWER sbe Project
 #
-# Contributors Listed Below - COPYRIGHT 2022
+# Contributors Listed Below - COPYRIGHT 2022,2023
+# [+] International Business Machines Corp.
 #
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -22,13 +23,54 @@
 #
 # IBM_PROLOG_END_TAG
 
-# _WDC_ACTING_OCC build flag:
+################################################################################
+# Adjustable build flags
+# ----------------------
+#
+# _WDC_ACTING_OCC:
 # - This flag controls the functionality of the Spinal images in that it makes
 #   the WOF Data Collector (WDC), which runs on Spinal's GPE0, behave like the 
 #   OCC(405) wrt sending DB0 to DCE and receiving VC1/3 from DCE/OCE.
 # - The flag ensures that all Spinal images have the same view of the IRQ 
 #   routing table in occhw_irq_config.h. This ensures consistent programming of 
 #   the OIRR regs a image boot time.
-# - To enable the functionality, set the flag :=1.
-# - For production code, leave the flag undefined.
-_WDC_ACTING_OCC:=1
+# - Usage:
+#   - To enable the functionality, set the flag :=1
+#   - To disable the functionality, leave the flag undefined.
+#   - For production code, leave the flag undefined.
+#
+# _BOOT_STANDALONE:
+# - This flag controls the programming of some key fields in the CE and PPE 
+#   image headers in the absence of p11_hcode_image_build, ie standalone mode.
+# - Usage:
+#   - To enable standalone mode, set the flag :=1
+#   - To disable standalone mode, leave the flag undefined.
+#   - For production code, leave the flag undefined.
+################################################################################
+
+_BOOT_STANDALONE:=1
+
+_WDC_ACTING_OCC:=
+
+
+################################################################################
+#
+#             *** BE CAREFUL MODIFYING THE FOLLOWING SECTION ***
+#
+################################################################################
+
+################################################################################
+# Conditioning the build flags
+# ----------------------------
+#
+# The following section ensures that an accidental setting of the above flags 
+# to, say, <flag>:=0 or <flag>==" " still results in the flag getting undefined.
+################################################################################
+
+ifneq ($(_BOOT_STANDALONE), 1)
+undefine _BOOT_STANDALONE
+endif
+
+ifneq ($(_WDC_ACTING_OCC), 1)
+undefine _WDC_ACTING_OCC
+endif
