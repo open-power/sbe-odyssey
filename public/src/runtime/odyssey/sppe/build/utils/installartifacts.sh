c2fc2014a94b0422a940a3ecc19286bd82bc28d9
akhis023 <akhis023@in.ibm.com>
2023-01-23 08:11:33 -0600

    Create odyssey debug tar as part of meson install step
    -Added SROM,Boot Loader and SPPE runtime debug tar files to
     odyssey debug tar
    -Can add other odyssey related debug files as and when needed
     into the tar
    
    JIRA: PFSBE-225
    Change-Id: I9bea19a346c4227ffe6c8c4b31e110c873e1b029
    Signed-off-by: akhis023 <akhis023@in.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/144277
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Rangaswamy H B . <rangaswamy.h.b@ibm.com>
    Reviewed-by: Ramesh I <rameshi1@in.ibm.com>
    Reviewed-by: AMIT J TENDOLKAR <amit.tendolkar@in.ibm.com>

---
+++ public/src/runtime/odyssey/sppe/build/utils/installartifacts.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: public/src/runtime/odyssey/sppe/build/utils/installartifacts.sh $
+#
+# OpenPOWER sbe Project
+#
+# Contributors Listed Below - COPYRIGHT 2023
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+
+mkdir -p ${MESON_BUILD_ROOT}/odyssey_debug_files_tools
+cp ${MESON_BUILD_ROOT}/public/src/runtime/odyssey/sppe/odyssey_sppe_DD1.dis \
+   ${MESON_BUILD_ROOT}/public/src/runtime/odyssey/sppe/odyssey_sppe_DD1.map \
+   ${MESON_BUILD_ROOT}/public/src/runtime/odyssey/sppe/odyssey_sppe_DD1.syms \
+   ${MESON_BUILD_ROOT}/public/src/runtime/odyssey/sppe/odyssey_sppe_DD1.attr.db \
+   ${MESON_BUILD_ROOT}/public/src/runtime/odyssey/sppe/odysseySppeStringFile_DD1 \
+   ${MESON_BUILD_ROOT}/public/src/onetime/odyssey/bldr/odyssey_bldr_DD1.dis \
+   ${MESON_BUILD_ROOT}/public/src/onetime/odyssey/bldr/odyssey_bldr_DD1.map \
+   ${MESON_BUILD_ROOT}/public/src/onetime/odyssey/bldr/odyssey_bldr_DD1.syms \
+   ${MESON_BUILD_ROOT}/public/src/onetime/odyssey/bldr/odysseyBldrStringFile_DD1 \
+   ${MESON_SOURCE_ROOT}/public/src/onetime/odyssey/srom/build/images/odysseySromStringFile_DD1 \
+   ${MESON_SOURCE_ROOT}/public/src/onetime/odyssey/srom/build/images/odyssey_srom_DD1.syms \
+   ${MESON_SOURCE_ROOT}/public/src/onetime/odyssey/srom/build/images/odyssey_srom_DD1.map \
+   ${MESON_SOURCE_ROOT}/public/src/onetime/odyssey/srom/build/images/odyssey_srom_DD1.dis \
+   ${MESON_BUILD_ROOT}/odyssey_debug_files_tools/ || exit 1
+
+# Copy simics related tools and utils into simics dir (This is required for HB)
+mkdir -p  ${MESON_BUILD_ROOT}/odyssey_debug_files_tools/simics/
+cp ${MESON_SOURCE_ROOT}/internal/src/runtime/odyssey/sppe/test/testcases/testUtil.py \
+   ${MESON_SOURCE_ROOT}/internal/src/tools/simics/sbestartupodystandalone.simics \
+   ${MESON_BUILD_ROOT}/odyssey_debug_files_tools/simics/ || exit 1
+
+tar -czvf ${MESON_INSTALL_PREFIX}/odyssey/odyssey_sbe_debug.tar.gz \
+    -C ${MESON_BUILD_ROOT} \
+    odyssey_debug_files_tools || exit 1
