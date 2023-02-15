3584a4dcb92904dc11fba48a6a71244983402609
Caleb Palmer <cnpalmer@us.ibm.com>
2023-02-15 08:41:03 -0600

    Add exp_collect_explorer files
    Change-Id: I04c37accf4ebfb9f88f90b1e9ed8c19ff9976255
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/145388
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Reviewed-by: Zane C Shelley <zshelle@us.ibm.com>
    Reviewed-by: Austin F Cui <austin.cui@ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/145713
    Reviewed-by: Team <hostboot@us.ibm.com>
    Reviewed-by: Sumit Kumar <sumit_kumar@in.ibm.com>

---
+++ public/src/import/public/memory/explorer/ffdc/exp_collect_explorer_saved_A_log.mk
@@ -0,0 +1,30 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: public/src/import/public/memory/explorer/ffdc/exp_collect_explorer_saved_A_log.mk $
+#
+# OpenPOWER sbe Project
+#
+# Contributors Listed Below - COPYRIGHT 2023
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+PROCEDURE=exp_collect_explorer_saved_A_log
+$(call ADD_MODULE_INCDIR,$(PROCEDURE),$(ROOTPATH)/public/memory/explorer/hwp/)
+lib$(PROCEDURE)_DEPLIBS+=exp_collect_explorer_log
+lib$(PROCEDURE)_DEPLIBS+=exp_fw_log_data
+
+$(call BUILD_PROCEDURE)
