9ed3ba76bcccd04b2bd80eb18d8b70cac32093df
Thi Tran <thi@us.ibm.com>
2023-02-20 09:16:48 -0500

    Import IO hwps from ekb
    EKB snapshot: https://rchgit01.rchland.ibm.com/gerrit1/#/c/146082/
    
    Key_Build=SPLIT_DIR_CHECK_IGNORE
    
    Change-Id: I42141ec7b5ce96c0f829d49b7a4015d06e366492
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/146112
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/146114
    Reviewed-by: Sumit Kumar <sumit_kumar@in.ibm.com>

--- public/src/runtime/odyssey/sppe/core/sbecmdexecutehwp.C
+++ public/src/runtime/odyssey/sppe/core/sbecmdexecutehwp.C
@@ -39,17 +39,16 @@ using namespace fapi2;
 static hwpMap_t hwpClass1PtrTbl[] =
           {
                HWP_MAP(istepLoadIOPPEwithOcmb, ody_omi_hss_ppe_load),   // 1.01
-               //HWP_MAP( NULL, NULL ),                                     // 1.01
                HWP_MAP( istepWithOcmb, ody_omi_hss_config ),              // 1.02
                HWP_MAP( istepWithOcmb, ody_omi_hss_ppe_start),            // 1.03
                HWP_MAP( istepWithOcmb, ody_omi_hss_bist_init ),           // 1.04
                HWP_MAP( istepWithOcmb, ody_omi_hss_bist_start ),          // 1.05
-               HWP_MAP( istepWithOcmb, ody_omi_hss_bist_poll ),           // 1.06
+               //HWP_MAP( istepWithOcmb, ody_omi_hss_bist_poll ),         // 1.06
+               HWP_MAP( NULL, NULL ),                                     // 1.06
                HWP_MAP( istepWithOcmb, ody_omi_hss_bist_cleanup),         // 1.07
                HWP_MAP( istepWithOcmb, ody_omi_hss_init ),                // 1.08
                HWP_MAP( istepWithOcmb, ody_omi_hss_dccal_start ),         // 1.09
-               //HWP_MAP( istepWithOcmb, ody_omi_hss_dccal_poll ),        // 1.10
-               HWP_MAP( NULL, NULL ),                                     // 1.10
+               HWP_MAP( istepWithOcmb, ody_omi_hss_dccal_poll ),          // 1.10
                HWP_MAP( istepWithOcmb, ody_omi_hss_tx_zcal ),             // 1.11
                HWP_MAP( istepWithOcmb, ody_omi_pretrain_adv ),            // 1.12
                HWP_MAP( istepWithOcmb, ody_omi_setup ),                   // 1.13
