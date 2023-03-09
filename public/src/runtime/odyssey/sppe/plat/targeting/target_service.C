f0957f51687616e59ff068ed5fd8ef3819e93d04
rajerpp1 <rajerpp1@in.ibm.com>
2023-03-09 05:17:44 -0600

    Adding function to change target state based on attribute
    - for target like TARGET_TYPE_MEM_PORT, TARGET_TYPE_DIMM etc, functional
      state is defined by attribute ATTR_SPPE_TARGET_STATE.
    - So if some one send a attr-update chipop, then we have to update the
      functional state based on the latest attribute value.
    
    Change-Id: I9c575b605269cc13bd967a29c1ee7081c0ba3718
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/147621
    Reviewed-by: Ramesh I <rameshi1@in.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: AMIT J TENDOLKAR <amit.tendolkar@in.ibm.com>

--- public/src/runtime/odyssey/sppe/plat/targeting/target_service.C
+++ public/src/runtime/odyssey/sppe/plat/targeting/target_service.C
@@ -30,6 +30,45 @@
 #include "plat_target_base.H"
 #include "target.H"
 #include "target_service.H"
+#include <fapi2_attribute_service.H>
 
 fapi2::target_service * g_platTarget = &fapi2::target_service::getInstance();
 fapi2::target_service fapi2::target_service::iv_instance;
+
+void fapi2::target_service::plat_TargetStateUpdateFromAttribute()
+{
+#define SBE_FUNC "plat_TargetStateUpdateFromAttribute "
+    SBE_ENTER(SBE_FUNC);
+
+    // set functional state for TARGET_TYPE_MEM_PORT, TARGET_TYPE_DIMM, TARGET_TYPE_TEMP_SENSOR
+    //    using attribute value ATTR_SPPE_TARGET_STATE
+    for(uint16_t j = 0; j < iv_targets.size(); j++)
+    {
+        uint8_t l_state = 0;
+        uint8_t l_targ_inst = iv_targets[j].getTargetInstance();
+        if(iv_targets[j].getTargetType() == fapi2::LOG_TARGET_TYPE_MEM_PORT)
+        {
+            l_state = fapi2::ATTR::TARGET_TYPE_MEM_PORT::ATTR_SPPE_TARGET_STATE[l_targ_inst];
+        }
+        else if(iv_targets[j].getTargetType() == fapi2::LOG_TARGET_TYPE_DIMM)
+        {
+            l_state = fapi2::ATTR::TARGET_TYPE_DIMM::ATTR_SPPE_TARGET_STATE[l_targ_inst];
+        }
+        else if(iv_targets[j].getTargetType() == fapi2::LOG_TARGET_TYPE_TEMP_SENSOR)
+        {
+            l_state = fapi2::ATTR::TARGET_TYPE_TEMP_SENSOR::ATTR_SPPE_TARGET_STATE[l_targ_inst];
+        }
+        else
+        {
+            // ATTR_SPPE_TARGET_STATE is not available for other targets
+            continue;
+        }
+
+        // set present if either TARGET_STATE_PRESENT or TARGET_STATE_FUNCTIONAL
+        iv_targets[j].setPresent(
+            (l_state == fapi2::TARGET_STATE_PRESENT) || (l_state == fapi2::TARGET_STATE_FUNCTIONAL));
+
+        iv_targets[j].setFunctional(l_state == fapi2::TARGET_STATE_FUNCTIONAL);
+    }
+#undef SBE_FUNC
+}
