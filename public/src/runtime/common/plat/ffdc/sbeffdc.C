d5e5cfbcca1f4ab06f205a62a1e200da7661b4f7
Srikantha Meesala <srikantha@in.ibm.com>
2023-03-21 06:16:40 -0500

    Part-2) SBE Traces, Attr dump enabled in a FFDC Packages Blobs in UE
    JIRA: PFSBE-122
    
    Change-Id: I01842c9248907ddd1275d8eaddcf7bfb9f0f318d
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/148222
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Akhilesh S <akhis023@in.ibm.com>
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>

--- public/src/runtime/common/plat/ffdc/sbeffdc.C
+++ public/src/runtime/common/plat/ffdc/sbeffdc.C
@@ -25,16 +25,16 @@
 #include "sbeFifoMsgUtils.H"
 #include "sbeffdc.H"
 #include "heap.H"
-#include "sbeutil.H"
 #include "plat_hwp_data_stream.H"
-#include <ffdc.H>
-#include <error_info.H>
 
 extern fapi2::pozFfdcData_t g_FfdcData;
 
+#define FIFO_BLOB_DATA_UPDATE( length, headerptr) \
+    CHECK_SBE_RC_AND_BREAK_IF_NOT_SUCCESS(ffdcPakageStream.put(length, headerptr)) \
+
 void SbeFFDCPackage::updateHWpackageDataHeader(void)
 {
-    #define SBE_FUNC "updateHWpakageDataHeader"
+    #define SBE_FUNC "updateHWpackageDataHeader"
     SBE_ENTER(SBE_FUNC);
 
     //As part of constructor ffdc package data header is updated with dump
@@ -46,9 +46,7 @@ void SbeFFDCPackage::updateHWpackageDataHeader(void)
     {
         // Package HW Data Blob with Field id and length
         iv_hwLocalBlob.field.fieldId = (uint16_t)SBE_FFDC_HW_DATA;
-        iv_hwLocalBlob.field.fieldLen =
-              ( fapi2::g_FfdcData.ffdcLength * (sizeof(fapi2::sbeFfdc_t)));
-        iv_hwLocalBlob.blobPtr = NULL;
+        iv_hwLocalBlob.field.fieldLen = fapi2::g_FfdcData.ffdcLength;
         iv_ffdcPackageHeader.lenInWords +=
                     ( BLOB_HEADER_SIZE_IN_WORDS +
                     (iv_hwLocalBlob.field.fieldLen/sizeof(uint32_t)));
@@ -73,6 +71,45 @@ void SbeFFDCPackage::updateHWpackageDataHeader(void)
     #undef SBE_FUNC
 }
 
+void SbeFFDCPackage::updateSBEpackageDataHeader(void)
+{
+    #define SBE_FUNC "updateSBEpackageDataHeader"
+    SBE_ENTER(SBE_FUNC);
+
+    //Update the pakage data header with dump fields configuration
+    iv_ffdcPackageHeader.lenInWords += PLAT_HEADER_SIZE_IN_WORDS;
+    if( iv_sbeDataHeader.dumpFields.get() & SBE_FFDC_TRACE_DATA )
+    {
+        // Package SBE Trace Data Blob with Field id and length
+        iv_sbeTraceBlob.field.fieldId = (uint16_t)SBE_FFDC_TRACE_DATA;
+        iv_sbeTraceBlob.field.fieldLen = SBE_FFDC_TRACE_DUMP_LENGTH;
+        iv_ffdcPackageHeader.lenInWords += ( sizeof(packageBlobField_t) +
+                        iv_sbeTraceBlob.field.fieldLen ) / sizeof(uint32_t);
+        SBE_INFO(SBE_FUNC" Enable SBE_FFDC_TRACE_DATA with lenInWords[%d]",
+                                      (uint32_t)iv_sbeTraceBlob.field.fieldLen);
+    }
+    if( iv_sbeDataHeader.dumpFields.get() & SBE_FFDC_ATTR_DATA )
+    {
+        // Package SBE Attr Data Blob with Field id and length
+        uint32_t *startAddr = &_attrs_start_;
+        uint32_t *endAddr = &_attrs_end_;
+        uint32_t len = ALIGN_8_BYTES(endAddr-startAddr);
+        SBE_INFO(SBE_FUNC " Attr Blob Length is 0x%08x", len);
+        iv_sbeAttrBlob.field.fieldId = (uint16_t)SBE_FFDC_ATTR_DATA;
+        iv_sbeAttrBlob.field.fieldLen = len * sizeof(uint32_t) ;
+        iv_ffdcPackageHeader.lenInWords += ( sizeof(packageBlobField_t) +
+                             iv_sbeAttrBlob.field.fieldLen ) / sizeof(uint32_t);
+        SBE_INFO(SBE_FUNC" Enable SBE_FFDC_ATTR_DATA with lenInWords[%d]",
+                                       (uint32_t)iv_sbeAttrBlob.field.fieldLen);
+    }
+    SBE_INFO(SBE_FUNC" SBE Trace and Attr  data in words[%d] ",
+                                     uint32_t(iv_ffdcPackageHeader.lenInWords));
+
+    return;
+    SBE_EXIT(SBE_FUNC);
+    #undef SBE_FUNC
+}
+
 uint32_t SbeFFDCPackage::createHwPackage( uint32_t &o_wordsSent, const bool isFifoData )
 {
     #define SBE_FUNC "createHwPackage "
@@ -91,47 +128,40 @@ uint32_t SbeFFDCPackage::createHwPackage( uint32_t &o_wordsSent, const bool isFi
 
         //Send FFDC package header
         length = sizeof(iv_ffdcPackageHeader) / sizeof(uint32_t);
-        uint32_t *headerptr = (uint32_t*)&iv_ffdcPackageHeader;
-        rc = ffdcPakageStream.put(length, headerptr);
-        CHECK_SBE_RC_AND_BREAK_IF_NOT_SUCCESS(rc);
+        FIFO_BLOB_DATA_UPDATE(length, (uint32_t*)&iv_ffdcPackageHeader);
         o_wordsSent += length;
 
         // package fields word update
-        uint32_t fields = iv_hwDataHeader.dumpFields.get();
-        headerptr = (uint32_t*)&fields;
-        rc = ffdcPakageStream.put(HW_HEADER_SIZE_IN_WORDS,headerptr);
-        CHECK_SBE_RC_AND_BREAK_IF_NOT_SUCCESS(rc);
+        uint32_t blobHeader = (uint32_t)iv_hwDataHeader.dumpFields.get();
+        FIFO_BLOB_DATA_UPDATE( HW_HEADER_SIZE_IN_WORDS,
+                               (uint32_t*)&blobHeader);
         o_wordsSent += HW_HEADER_SIZE_IN_WORDS;
         SBE_INFO(" packageFields Data: 0x%08X", iv_hwDataHeader.dumpFields.get());
 
         if( iv_hwLocalBlob.field.fieldId & SBE_FFDC_HW_DATA)
         {
-            fields = iv_hwLocalBlob.field.get();
-            headerptr = (uint32_t*)&fields;;
-            rc = ffdcPakageStream.put(HW_HEADER_SIZE_IN_WORDS, headerptr);
-            CHECK_SBE_RC_AND_BREAK_IF_NOT_SUCCESS(rc);
+            FIFO_BLOB_DATA_UPDATE( HW_HEADER_SIZE_IN_WORDS,
+                                  (uint32_t*)&iv_hwLocalBlob.field);
             o_wordsSent += BLOB_HEADER_SIZE_IN_WORDS;
-
-            SBE_INFO(" fields and PackageDataId.fieldLen [ 0x%08X ]", fields);
+            SBE_INFO(SBE_FUNC" fields and PackageDataId.fieldLen [ 0x%08X ]",
+                                                    iv_hwLocalBlob.field.get());
             if(iv_hwLocalBlob.field.fieldLen != 0)
             {
-                headerptr = (uint32_t*)(fapi2::g_FfdcData.ffdcDataPtr);
                 length = iv_hwLocalBlob.field.fieldLen / sizeof(uint32_t);
-                SBE_INFO(" SBE_FFDC_HW_DATA data length[%d]", length);
-                rc = ffdcPakageStream.put(length, headerptr);
-                CHECK_SBE_RC_AND_BREAK_IF_NOT_SUCCESS(rc);
+                SBE_INFO(SBE_FUNC" SBE_FFDC_HW_DATA data length[%d]", length);
+                FIFO_BLOB_DATA_UPDATE( length,
+                                    (uint32_t*)(fapi2::g_FfdcData.ffdcDataPtr));
             }
         }
         o_wordsSent += length;
 
         if( iv_hwRegBlob.field.fieldId & SBE_FFDC_REG_DATA)
         {
-            fields = (uint32_t)iv_hwRegBlob.field.get();
-            headerptr = (uint32_t*)&fields;
-            rc = ffdcPakageStream.put(HW_HEADER_SIZE_IN_WORDS, headerptr);
-            CHECK_SBE_RC_AND_BREAK_IF_NOT_SUCCESS(rc);
+            FIFO_BLOB_DATA_UPDATE( HW_HEADER_SIZE_IN_WORDS,
+                                  (uint32_t*)&iv_hwRegBlob.field);
             o_wordsSent += BLOB_HEADER_SIZE_IN_WORDS;
-            SBE_INFO(" SBE_FFDC_REG_DATA:PackageDataId.fieldLen [0x%08X ]", fields);
+            SBE_INFO(SBE_FUNC " SBE_FFDC_REG_DATA data length[0x%04X]",
+                                         (uint16_t)iv_hwRegBlob.field.fieldLen);
             // @TODO Enable HW Reg data
         }
         SBE_INFO(SBE_FUNC " Number of words sent [%d], lenInWords[%d]",
@@ -140,6 +170,74 @@ uint32_t SbeFFDCPackage::createHwPackage( uint32_t &o_wordsSent, const bool isFi
     } while(false);
     // Clean up scratch data
     Heap::get_instance().scratch_free(fapi2::g_FfdcData.ffdcDataPtr);
+    SBE_EXIT(SBE_FUNC);
+    return rc;
+    #undef SBE_FUNC
+}
+
+uint32_t SbeFFDCPackage::createSbePackage( uint32_t &o_bytesSent, const bool isFifoData )
+{
+    #define SBE_FUNC "createSbePakage "
+    SBE_ENTER(SBE_FUNC);
+    uint32_t rc = SBE_SEC_OPERATION_SUCCESSFUL;
+    uint32_t length = 0;
+    //reset sent bytes
+    o_bytesSent = 0;
+    do
+    {
+        //Update package headers with dump fields configuration
+        updateSBEpackageDataHeader();
+
+        // Create the fifo ostream class pointing to the scratch space values
+        fapi2::sbefifo_hwp_data_ostream ffdcPakageStream;
+
+        //Send FFDC package header
+        length = sizeof(iv_ffdcPackageHeader) / sizeof(uint32_t);
+        FIFO_BLOB_DATA_UPDATE( length, (uint32_t*)&iv_ffdcPackageHeader );
+        o_bytesSent += length;
+
+        // package fields word update
+        length = PLAT_HEADER_SIZE_IN_WORDS - BLOB_HEADER_SIZE_IN_WORDS;
+        FIFO_BLOB_DATA_UPDATE( length, (uint32_t*)&iv_sbeDataHeader );
+        SBE_INFO("packageFields Data: 0x%08X",iv_sbeDataHeader.dumpFields.get());
+        o_bytesSent += length;
+
+        uint32_t blobHeader = (uint32_t)iv_sbeDataHeader.dumpFields.get();
+        FIFO_BLOB_DATA_UPDATE( BLOB_HEADER_SIZE_IN_WORDS,
+                               (uint32_t*)&blobHeader);
+        o_bytesSent += BLOB_HEADER_SIZE_IN_WORDS;
+
+        if( iv_sbeTraceBlob.field.fieldId & SBE_FFDC_TRACE_DATA)
+        {
+            FIFO_BLOB_DATA_UPDATE( BLOB_HEADER_SIZE_IN_WORDS,
+                               (uint32_t*)&iv_sbeTraceBlob.field );
+            o_bytesSent += BLOB_HEADER_SIZE_IN_WORDS;
+            SBE_INFO(SBE_FUNC " trace fields and PackageData length[0x%08X]",
+                                                   iv_sbeTraceBlob.field.get());
+            length = iv_sbeTraceBlob.field.fieldLen / sizeof(uint32_t);
+            SBE_INFO(" SBE_FFDC_TRACE_DATA data length[%d]", length);
+            FIFO_BLOB_DATA_UPDATE( length, (uint32_t*)(G_PK_TRACE_BUF));
+        }
+        o_bytesSent += length;
+
+        if( iv_sbeAttrBlob.field.fieldId & SBE_FFDC_ATTR_DATA)
+        {
+            FIFO_BLOB_DATA_UPDATE( BLOB_HEADER_SIZE_IN_WORDS,
+                               (uint32_t*)&iv_sbeAttrBlob.field );
+            o_bytesSent += BLOB_HEADER_SIZE_IN_WORDS;
+
+            SBE_INFO(SBE_FUNC "Attr field and PackageData length [0x%08X]",
+                                                   iv_sbeAttrBlob.field.get());
+            length = iv_sbeAttrBlob.field.fieldLen / sizeof(uint32_t);
+            SBE_INFO(" SBE_FFDC_ATTR_DATA data length[%d]", length);
+            FIFO_BLOB_DATA_UPDATE( length, (uint32_t *)&_attrs_start_);
+        }
+        SBE_INFO(SBE_FUNC " Number of words sent [%d], lenInWords[%d]",
+                 o_bytesSent, iv_ffdcPackageHeader.lenInWords);
+        o_bytesSent += length;
+        SBE_INFO(" packageFields size[%d] ", o_bytesSent);
+    } while(false);
+
     SBE_EXIT(SBE_FUNC);
     return rc;
     #undef SBE_FUNC
@@ -163,13 +261,16 @@ uint32_t sendFFDCOverFIFO( const uint32_t i_fieldsConfig,
                      i_fieldsConfig & SBE_FFDC_ALL_HW_DATA);
             SbeFFDCPackage objFFDC(i_fieldsConfig & SBE_FFDC_ALL_HW_DATA);
             objFFDC.createHwPackage( wordsCount, i_isFifoData);
-            o_wordsSent += wordsCount;;
+            o_wordsSent += wordsCount;
         }
 
         if( i_fieldsConfig & SBE_FFDC_ALL_PLAT_DATA)
         {
-            // @TODO Enable Sbe Ffdc Package 
-            // JIRA: PFSBE-122
+            SbeFFDCPackage objFFDC(i_fieldsConfig & SBE_FFDC_ALL_PLAT_DATA);
+            SBE_INFO(SBE_FUNC " Creating  ALL SBE FFDC Pakage[%d]",
+                     i_fieldsConfig & SBE_FFDC_ALL_PLAT_DATA);
+            objFFDC.createSbePackage( wordsCount, i_isFifoData );
+            o_wordsSent += wordsCount;
         }
         o_wordsSent = wordsCount;
 
