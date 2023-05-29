6c4cb3839c52aee9f35c334934367c7ad2c3ff04
rajerpp1 <rajerpp1@in.ibm.com>
2023-05-29 02:21:28 -0500

    syncing attrtool from ekb-p10
    Change-Id: Ia6634ac0b01901cd09bf5397d5fc529d19cd4ab3
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/152620
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: RAJESWARAN THILLAIGOVINDAN <rajeswgo@in.ibm.com>
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>

--- public/src/runtime/tools/attributes/attrdb.py
+++ public/src/runtime/tools/attributes/attrdb.py
@@ -23,13 +23,42 @@
 # permissions and limitations under the License.
 #
 # IBM_PROLOG_END_TAG
+
+#standard library imports
+
 from xml.dom.minidom import Element
 import xml.etree.ElementTree as etree
 from textwrap import dedent
 from collections import namedtuple
-from attrtoolutils import *
 import json
 import copy
+
+#attrltool library imports
+from attrtoolutils import *
+import attrdatatype
+
+###############################################################
+# attrtool helper classess
+#
+# The attribute definition XML files are parsed and stored in
+# attrdb. As SBE is memonry constrained, not all the attributes
+# defined are supported. For an attribute to be supported by,
+# SBE, it must be listed in the SBE attribute XML files which
+# are called as filter XML in SBE parlance.
+#
+# The python classes:
+#   RealAttribute - represents an attribute element in the
+#                   attribute definition XML.
+#   ECAttribute   - attribute element having a child element
+#                   called chipEcFeature
+#   AttributeDB   - represents a collection of all attributes
+#                   both RealAttribute and ECAttribute
+#   SBEAttributes - represents a collection of all SBE supported
+#                   attributes
+#   SBEnry        - represents an attribute supported by SBE
+#   TargetEntry   - represents a child of SBE attribute to
+#                   provide values for compile time initialization
+###############################################################
 
 TargetTypeInfo = namedtuple("TargetTypeInfo", "ntargets")
 
@@ -46,13 +75,35 @@ class RealAttribute(object):
     Class to parse attribute from XML which need a real storage location
     """
 
-    def __init__(self, db:"AttributeDB", node:Element):
+    class SbeAttrSync(object):
+        '''
+        class to parse sbeAttrSync tag
+        '''
+        def __init__(self, sync_node:Element, i_ekb_targets:'list[str]') -> None:
+            self.to_sbe :bool = (sync_node.attrib.get("toSBE") == "1")
+            self.from_sbe :bool = (sync_node.attrib.get("fromSBE") == "1")
+
+            _chip_types :str = sync_node.attrib.get("chipTypes")
+            self.chip_types :list[str] = _chip_types.split(',')
+
+            _target_types :str = sync_node.find("targetTypes").text
+            self.target_types :list[str] = [targ.strip().upper() for targ in _target_types.split(',')]
+
+            if(not (self.to_sbe or self.from_sbe)):
+                raise ParseError("sbeAttrSync doesn't enabling either to_sbe nor from_sbe")
+
+            if(not set(self.target_types).issubset(set(i_ekb_targets))):
+                raise ParseError("sbeAttrSync has target types not supported for this attribute")
+
+
+    def __init__(self, db:"AttributeDB", node:Element, parse_sync_tag:bool):
         self.db = db
         self.sbe_entry:"SBEEntry" = None
+        self.attr_sync = None
         self.unsupported_attribute = False
 
-        name, target_type, value_type, description, enum_values, array_dims = (
-            node.find(tag) for tag in ("id", "targetType", "valueType", "description", "enum", "array"))
+        name, target_type, value_type, description, enum_values, array_dims, attr_sync = (
+            node.find(tag) for tag in ("id", "targetType", "valueType", "description", "enum", "array", "sbeAttrSync"))
 
         if name is None:
             raise ParseError("Unnamed attribute")
@@ -74,7 +125,14 @@ class RealAttribute(object):
                 self.sbe_target_type.append(targ)
             self.ekb_target_type.append(targ)
 
-        if len(self.sbe_target_type) == 0:
+        if(parse_sync_tag and (attr_sync != None)):
+            try:
+                self.attr_sync = RealAttribute.SbeAttrSync(attr_sync, self.sbe_target_type)
+            except ParseError as e:
+                raise ParseError("SbeAttrSync parsing failed for " + self.name) from e
+
+
+        if((len(self.sbe_target_type) == 0) and (self.attr_sync == None)):
             self.unsupported_attribute = True
             return
 
@@ -88,10 +146,20 @@ class RealAttribute(object):
             # TODO: Be strict about commas
             self.array_dims = [int(dim, 0) for dim in array_dims.text.replace(",", " ").split()]
 
+        self.tot_size = attrdatatype.VALUE_TYPES[self.value_type.lower()].size
+        for dim in self.array_dims:
+            self.tot_size *= dim
+
         if node.find("writeable") != None:
             self.writeable = True
         else:
             self.writeable = False
+            if(self.attr_sync != None):
+                # 1. Since it's a read-only attribute, we expect no change in this value in the SBE after
+                #    the attribute push.
+                # 2. And, the HWP will apply the value to the host by calling FAPI_ATTR_SET, but this macro
+                #    will not be available for read-only attributes.
+                assert (self.attr_sync.from_sbe == False), "fromSBE is set for read-only attribute " + self.name
 
         if node.find("platInit") != None:
             self.platinit = True
@@ -99,6 +167,12 @@ class RealAttribute(object):
             self.platinit = False
 
         # TODO: Support tags <default> and <initToZero/>
+
+    def fromSbeSync(self, i_chip_type, i_target) -> None :
+        return ((self.attr_sync != None) and
+            (i_chip_type in self.attr_sync.chip_types) and
+            (i_target in self.attr_sync.target_types) and
+            (self.attr_sync.from_sbe))
 
     def validate_and_return_sbe_attribute(self, sbe_value:"SBEEntry") -> "SBEEntry":
         if sbe_value != None:
@@ -198,19 +272,111 @@ class AttributeDB(object):
     """
     TARGET_TYPES : 'dict[str, TargetTypeInfo]' = dict()
 
-    def __init__(self, I_target_json_path : str):
-        with open(I_target_json_path, 'r') as fp:
+    '''
+    An object of this class will be instantiated for two purposes.
+    1. HWP auto-generation: here we will be only supplied by ekb XMLs, and it will have information about different chip's attributes.
+       So we need an input (i_chip_type) to select the attributes which are interesting to us.
+    2. SBE source code generation: here we will be supplied with ekb XMLs and SBE XMLs, and we have to combine the information in
+       these two to generate the source code. Here SBE XML will have information, which all attributes are interesting to us.
+       So "i_chip_type" is not required here.
+    '''
+    def __init__(self, i_target_json_path : str, i_chip_type : str = None):
+        with open(i_target_json_path, 'r') as fp:
             AttributeDB.TARGET_TYPES =json.load(
                     fp, object_hook= lambda d : targetTypeInfoLoader(**d))
-        self.attributes : 'dict[str, RealAttribute|ECAttribute]' = dict()
 
-    def load_xml(self, fname):
+        self.attributes : 'dict[str, RealAttribute|ECAttribute]' = dict()
+        self.chip_type = i_chip_type
+        self.sync_attr_tabulated = False
+        self.to_sbe_list :dict[str,RealAttribute] = dict()
+        self.from_sbe_list :dict[str,RealAttribute] = dict()
+        # Attribute update header size is 4 bytes
+        self.attr_upd_blob_size = 4
+
+    def load_xml(self, fname, parse_sync_tag = False):
         root = etree.parse(fname).getroot()
         for elem in root.iter("attribute"):
-            attr = ECAttribute(self, elem) if elem.find("chipEcFeature") else RealAttribute(self, elem)
+            attr = ECAttribute(self, elem) if elem.find("chipEcFeature") else RealAttribute(self, elem, parse_sync_tag)
+
             if attr.name in self.attributes:
                 raise ParseError("Duplicate attribute " + attr.name)
             self.attributes[attr.name] = attr
+
+    '''
+    Arrange the attributes into dictionaries to_sbe_list and from_sbe_list, with target as keys.
+    Also calculate the attribute_update_blob size for the sync attributes.
+    '''
+    def tabulateSyncAttributes(self):
+        if(self.sync_attr_tabulated):
+            return
+
+        for attr in self.attributes.values():
+            if(not isinstance(attr, RealAttribute)):
+                continue
+
+            if((attr.attr_sync != None) and
+                (attr.attr_sync.to_sbe) and
+                (self.chip_type in attr.attr_sync.chip_types)):
+
+                for targ in attr.attr_sync.target_types:
+                    target_section_size = 0
+                    if(targ not in self.to_sbe_list.keys()):
+                        self.to_sbe_list[targ] = []
+                        # target header size is 4 bytes
+                        target_section_size += 4
+
+                    self.to_sbe_list[targ].append(attr)
+                    self.calculateBlobSizeForAttribute(
+                        targ, attr.tot_size, target_section_size);
+
+            # in apply_attribute HWP, we need all the attributes
+            #   since we have to ignore the attributes in the xml but
+            #   fromSbe is not set, and we have to throw error for
+            #   attributes thats not present in any xml
+            for targ in attr.sbe_target_type:
+                if(targ not in self.from_sbe_list.keys()):
+                    self.from_sbe_list[targ] = []
+                self.from_sbe_list[targ].append(attr)
+
+        self.sync_attr_tabulated = True
+
+    def calculateBlobSizeForAttribute(self, i_targ, i_attr_size, i_target_section_size):
+        # The actual size of the attribute row :
+        # The size of the attribute id   = 4 bytes +
+        # The number of bytes to specify
+        #     attribute data size        = 2 bytes +
+        # The number of bytes used to
+        #     specify the co-ordinates
+        #     row,col,hgt,1 byte reserved= 4 bytes +
+        # The number of bytes to store actual data size
+
+        attr_actual_data_size = 10 + i_attr_size
+        attr_aligned_data_size= (attr_actual_data_size+7) & (~7)
+
+        i_target_section_size  += attr_aligned_data_size
+
+        i_target_section_size *= self.TARGET_TYPES[i_targ].ntargets
+
+        self.attr_upd_blob_size += i_target_section_size
+
+    def getToSbeSystemAttributes(self):
+        self.tabulateSyncAttributes()
+
+        try:
+            return self.to_sbe_list['TARGET_TYPE_SYSTEM']
+        except KeyError:
+            # return empty list, if no entry for this target
+            return []
+
+    def getToSbeOdysseyChipAttributes(self):
+        self.tabulateSyncAttributes()
+
+        try:
+            assert self.chip_type == "ody", "This function is valid only for ody chip type"
+            return self.to_sbe_list['TARGET_TYPE_OCMB_CHIP']
+        except KeyError:
+            # return empty list, if no entry for this target
+            return []
 
 class TargetEntry(object):
     """
