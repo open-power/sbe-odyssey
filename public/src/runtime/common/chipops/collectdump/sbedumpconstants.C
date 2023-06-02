10e694e4a50325e01ce51beb321257b4ba8ec3ac
in0029y2 <anuj.tiwari2@ibm.com>
2023-06-02 00:35:39 -0500

    SBE Dump Chipop infra and base changes
    - Base code and infra changes
    
    JIRA: PFSBE-149
    Change-Id: I17d9cd27260062f14778017bfe427e4ec1944de8
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/152753
    Reviewed-by: Akhilesh S <akhis023@in.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>

---
+++ public/src/runtime/common/chipops/collectdump/sbedumpconstants.C
@@ -0,0 +1,72 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: public/src/runtime/common/chipops/collectdump/sbedumpconstants.C $ */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2023                             */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+#include "sbedumpconstants.H"
+
+DUMPTYPEMAP dumpTypeMap[4] = {
+    {SBE_DUMP_TYPE_SCS , DUMP_TYPE_SCS},
+    {SBE_DUMP_TYPE_MPIPL , DUMP_TYPE_MPIPL},
+    {SBE_DUMP_TYPE_PERF , DUMP_TYPE_PERF},
+    {SBE_DUMP_TYPE_HB , DUMP_TYPE_HB}
+};
+
+//Index mapped table wrt commandTypes enum
+//for storing the size of the genericHdctRow_t
+//for a particular command type.
+//Write NULL for commands that are not used in HDCT.txt
+size_t genericHdctRowSize_table[NO_OF_CMD_TYPES] = {
+    CMD_TYPE_NOT_USED,
+    genericHdctRowSize(genericHdctRow_t,cmdGetScom),
+    genericHdctRowSize(genericHdctRow_t,cmdPutScom),
+    CMD_TYPE_NOT_USED,
+    CMD_TYPE_NOT_USED,
+    genericHdctRowSize(genericHdctRow_t,cmdGetRing),
+    CMD_TYPE_NOT_USED,
+    genericHdctRowSize(genericHdctRow_t,cmdStopClocks),
+    CMD_TYPE_NOT_USED,
+    CMD_TYPE_NOT_USED,
+    CMD_TYPE_NOT_USED,
+    genericHdctRowSize(genericHdctRow_t,cmdTraceArray),
+    genericHdctRowSize(genericHdctRow_t,cmdFastArray),
+    CMD_TYPE_NOT_USED,
+    genericHdctRowSize(genericHdctRow_t,cmdGetSram)
+};
+
+uint16_t getEquivDumpType(uint8_t reqDumpType)
+{
+    #define SBE_FUNC " getEquivDumpType "
+
+    uint8_t count = (sizeof(dumpTypeMap)/sizeof(dumpTypeMap[0]));
+    uint16_t hdctDumpTypeMap = 0;
+    for(uint8_t i = 0; i < count; i++)
+    {
+       if( dumpTypeMap[i].dumpRequestReceived == reqDumpType)
+       {
+            //Return as per HDCT.bin dump type format.
+            hdctDumpTypeMap = (1 << dumpTypeMap[i].hdctDumpType);
+       }
+    }
+
+    return hdctDumpTypeMap;
+    #undef SBE_FUNC
+}
---
+++ public/src/runtime/odyssey/sppe/core/sbecollectdumpwrap.C
@@ -0,0 +1,82 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: public/src/runtime/odyssey/sppe/core/sbecollectdumpwrap.C $   */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2023                             */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+#include "sbecollectdump.H"
+
+using namespace fapi2;
+
+#define DUMP_HDR_CHIP_TYPE_TO_ENCODE 0x7
+
+/****************** getDumpHdrChipTypeToEncode *****************/
+uint8_t sbeCollectDump::getDumpHdrChipTypeToEncode()
+{
+    return (uint8_t)DUMP_HDR_CHIP_TYPE_TO_ENCODE;
+}
+
+/************************ getTargetList ***********************/
+void sbeCollectDump::getTargetList(std::vector<fapi2::plat_target_handle_t> &o_targetList)
+{
+    #define SBE_FUNC "getTargetList"
+    SBE_ENTER(SBE_FUNC);
+    fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP> l_chip =  g_platTarget->plat_getChipTarget();
+    switch(iv_tocRow.hdctHeader.chipUnitType)
+    {
+        case CHIP_UNIT_TYPE_CHIP:
+        case CHIP_UNIT_TYPE_UNKNOWN:
+        {
+            o_targetList.push_back(l_chip);
+            SBE_DEBUG(SBE_FUNC "PROC: [0x%08X]", l_chip.get());
+            break;
+        }
+        case CHIP_UNIT_TYPE_PERV:
+        {
+            for( auto& target : l_chip.getChildren<fapi2::TARGET_TYPE_PERV>(
+                static_cast<fapi2::TargetFilter>(
+                        fapi2::TARGET_FILTER_TP     |
+                        fapi2::TARGET_FILTER_MC))
+                )
+            {
+                o_targetList.push_back(target);
+                SBE_DEBUG(SBE_FUNC " PERV: [0x%08X]", target.get());
+            }
+            break;
+        }
+        case CHIP_UNIT_TYPE_MEM_PORT:
+        {
+            for( auto& target : l_chip.getChildren<fapi2::TARGET_TYPE_MEM_PORT>())
+            {
+                o_targetList.push_back(target);
+            }
+            break;
+        }
+        default:
+        {
+            SBE_ERROR(SBE_FUNC " PROC: [0x%08X] command Id [0x%02X] Type[0x%02X] not supported",
+                        l_chip.get(),
+                        (uint8_t)iv_tocRow.hdctHeader.chipUnitType,
+                        (uint8_t)iv_tocRow.hdctHeader.cmdType);
+            break;
+        }
+    }
+    #undef SBE_FUNC
+}
