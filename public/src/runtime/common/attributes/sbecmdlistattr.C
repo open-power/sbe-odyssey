7e1c052d7ec082582521ea606732f7768c737863
RAJESWARAN THILLAIGOVINDAN <rajeswgo@in.ibm.com>
2023-06-06 09:02:13 -0500

    Common code refactor : attributes chip-op
    This story is about moving attribute dump, list, and update
    chip-operations to common space.
    
    JIRA: PFSBE-327
    Change-Id: I8beaddf70fa82af4f543906818d0a4a8e52992b1
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/152934
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Akhilesh S <akhis023@in.ibm.com>
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>

--- public/src/runtime/common/attributes/sbecmdlistattr.C
+++ public/src/runtime/common/attributes/sbecmdlistattr.C
@@ -1,12 +1,11 @@
 /* IBM_PROLOG_BEGIN_TAG                                                   */
 /* This is an automatically generated prolog.                             */
 /*                                                                        */
-/* $Source: public/src/runtime/odyssey/sppe/core/sbecmdupdateattr.C $     */
+/* $Source: public/src/runtime/common/attributes/sbecmdlistattr.C $       */
 /*                                                                        */
 /* OpenPOWER sbe Project                                                  */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2022,2023                        */
-/* [+] International Business Machines Corp.                              */
+/* Contributors Listed Below - COPYRIGHT 2023                             */
 /*                                                                        */
 /*                                                                        */
 /* Licensed under the Apache License, Version 2.0 (the "License");        */
@@ -24,93 +23,75 @@
 /* IBM_PROLOG_END_TAG                                                     */
 #include "sbeFifoMsgUtils.H"
 #include "chipop_struct.H"
-#include "sbecmdupdateattr.H"
+#include "sbecmdlistattr.H"
 #include "sbe_sp_intf.H"
 #include "sbeglobals.H"
 #include "heap.H"
-#include "attribute_override.H"
-#include "target.H"
-#include "attribute_table.H"
+#include "attribute_list.H"
 
-uint32_t sbeUpdateAttr(uint8_t *i_pArg)
+uint32_t sbeCmdListAttr(uint8_t *i_pArg)
 {
-    #define SBE_FUNC " sbeUpdateAttr "
+    #define SBE_FUNC " sbeCmdListAttr "
     SBE_ENTER(SBE_FUNC);
 
-    uint32_t l_rc = SBE_SEC_OPERATION_SUCCESSFUL,
-             l_fifoRc = SBE_SEC_OPERATION_SUCCESSFUL;
+    uint32_t l_rc = SBE_SEC_OPERATION_SUCCESSFUL;
     uint32_t l_len = 0;
+    uint32_t l_fifoRc =0;
     sbeFifoType type;
     sbeRespGenHdr_t respHdr;
     respHdr.init();
-    void *l_inOutBuffer = nullptr;
+    void *l_outBuffer = nullptr;
     do
     {
         chipOpParam_t* configStr = (struct chipOpParam*)i_pArg;
         type = static_cast<sbeFifoType>(configStr->fifoType);
-
-        SBE_DEBUG(SBE_FUNC "Fifo Type is:[%02X]",type);
-
-        // Get the length of payload
-        // Length is not part of chipop. So take length from total length
-        l_len = SBE_GLOBAL->sbeFifoCmdHdr.len -
-                        sizeof(SBE_GLOBAL->sbeFifoCmdHdr)/sizeof(uint32_t);
-
-        SBE_INFO(SBE_FUNC"Expected length of the payload(in words) : %d",
-                        l_len);
-
-        // The size of the buffer to be allocated in bytes.
-        // For that, multiply the number of words by 4.
-        l_inOutBuffer = Heap::get_instance().scratch_alloc(l_len*4);
-        if (l_inOutBuffer == nullptr)
+        l_rc = sbeUpFifoDeq_mult(l_len, NULL, true, false, type);
+        // If FIFO access failure
+        if(l_rc)
         {
-            SBE_ERROR(SBE_FUNC"scratch allocation request for [%d] bytes "
-                        "failed.", (l_len*4));
+            l_fifoRc = l_rc;
+            SBE_ERROR(SBE_FUNC"l_fifoRc upfifo : 0x%08x", l_fifoRc);
+            break;
+        }
+        SBE_INFO(SBE_FUNC "Fifo Type is:[%02X]",type);
+        uint32_t attrListSize = fapi2::ATTR::ListResponseBuffer::getExpectedHeapSize();
+        l_outBuffer = Heap::get_instance().scratch_alloc(attrListSize);
+        SBE_INFO(SBE_FUNC " output buffer = 0x%08x", l_outBuffer);
+        if (l_outBuffer == nullptr)
+        {
+            SBE_ERROR(SBE_FUNC"scratch allocation request for [%d] bytes failed.", attrListSize);
             l_rc=SBE_SEC_HEAP_SPACE_FULL_FAILURE;
             break;
         }
-
-        l_fifoRc = sbeUpFifoDeq_mult (l_len, (uint32_t *)l_inOutBuffer, true,
-                                        false, type);
-        CHECK_SBE_RC_AND_BREAK_IF_NOT_SUCCESS(l_fifoRc);
-
-        uint32_t  l_respPackSize = 0;
-        uint32_t  l_bufSizeInBytes = l_len*4;
-
-        l_rc = fapi2::ATTR::applyOverride(l_inOutBuffer,l_bufSizeInBytes,l_respPackSize);
+        l_rc = fapi2::ATTR::listAttribute(l_outBuffer);
         CHECK_SBE_RC_AND_BREAK_IF_NOT_SUCCESS(l_rc);
-
-        SBE_INFO("Calling plat_TargetStateUpdateFromAttribute()");
-        g_platTarget->plat_TargetStateUpdateFromAttribute();
-
-        uint32_t len2enqueue = l_respPackSize / sizeof(uint32_t);
-
-        l_fifoRc = sbeDownFifoEnq_mult (len2enqueue, (uint32_t *)l_inOutBuffer,
-                                        type);
+        uint32_t len2enqueue = attrListSize / 4;
+        SBE_INFO(SBE_FUNC "len2Enqueue = 0x%08x", len2enqueue);
+        l_fifoRc = sbeDownFifoEnq_mult (len2enqueue, (uint32_t *)l_outBuffer, type);
         CHECK_SBE_RC_AND_BREAK_IF_NOT_SUCCESS(l_fifoRc);
 
     } while(false);
 
     // scratch_free() will check if the input pointer
     // is nullptr before calling free
-    Heap::get_instance().scratch_free(l_inOutBuffer);
-
-
+    Heap::get_instance().scratch_free(l_outBuffer);
     if (l_fifoRc == SBE_SEC_OPERATION_SUCCESSFUL)
     {
         if(l_rc != SBE_SEC_OPERATION_SUCCESSFUL)
         {
-            respHdr.setStatus( SBE_PRI_GENERIC_EXECUTION_FAILURE, l_rc );
+            respHdr.setStatus( SBE_PRI_GENERIC_EXECUTION_FAILURE,
+                            l_rc );
         }
+
         l_fifoRc = sbeDsSendRespHdr(respHdr, NULL, type);
         if ( l_fifoRc != SBE_SEC_OPERATION_SUCCESSFUL )
         {
-            SBE_ERROR(SBE_FUNC"Attribute update chip-op send response "
+            SBE_ERROR(SBE_FUNC"Attribute List chip-op send response "
                         "header failed.RC=0x%08X",l_fifoRc);
         }
     }
-
     SBE_EXIT(SBE_FUNC);
     return l_fifoRc;
     #undef SBE_FUNC
+
 }
