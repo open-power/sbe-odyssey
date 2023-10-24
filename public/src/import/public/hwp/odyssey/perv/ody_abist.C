b1c9d01e318d7798bbc99a3537ba57ea8b4c9ea4
Joachim Fenkes <fenkes@de.ibm.com>
2023-10-24 11:31:40 -0400

    Remove product ABIST/LBIST calls on Odyssey
    There is no product ABIST and LBIST defined for Odyssey and DFT are using
    poz_bist directly, so disable the odyssey specific product BIST calls to
    avoid runtime errors due to missing bist.pak content.
    
    Change-Id: I63426999308059718e75085dc930269e17b61ae6
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/159184
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel Kiss <kiss@de.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Joachim Fenkes <fenkes@de.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/159187
    Dev-Ready: Kahn C Evans <kahnevan@us.ibm.com>
    Reviewed-by: Team <hostboot@us.ibm.com>
    Reviewed-by: Sumit Kumar <sumit_kumar@in.ibm.com>

--- public/src/import/public/hwp/odyssey/perv/ody_abist.C
+++ public/src/import/public/hwp/odyssey/perv/ody_abist.C
@@ -31,94 +31,11 @@
 //------------------------------------------------------------------------------
 
 #include "ody_abist.H"
-#include "poz_bist.H"
-#include <poz_perv_common_params.H>
 
 using namespace fapi2;
 
-static const bist_params ody_abist_params =
-{
-    BIST_PARAMS_CURRENT_VERSION,        ///< BIST_PARAMS_VERSION
-
-    bist_params::SCAN0              |   ///< 0x0200
-    bist_params::RING_SETUP         |   ///< 0x0080
-    bist_params::RING_PATCH         |   ///< 0x0040
-    bist_params::REG_SETUP          |   ///< 0x0020
-    bist_params::GO                 |   ///< 0x0010
-    bist_params::POLL               |   ///< 0x0008
-    bist_params::REG_CLEANUP        |   ///< 0x0004
-    bist_params::COMPARE,               ///< 0x0002
-
-    bist_params::ABIST_NOT_LBIST    |   ///< 0x80000000
-    bist_params::POLL_ABIST_DONE    |   ///< 0x08000000
-    bist_params::ASSERT_ABIST_DONE  |   ///< 0x04000000
-    bist_params::SCAN0_ARY_FILL     |   ///< 0x00400000
-    bist_params::SYS_BIST_PAK,          ///< 0x00200000
-
-    /// 0x0 for chiplets to let hotplug decide which chiplets to BIST
-    0x0000000000000000,                 ///< chiplets
-    0x0000000000000000,                 ///< uc_go_chiplets
-    135000000,                          ///< opcg_count
-    0x20000,                            ///< idle_count
-    0,                                  ///< linear_stagger
-    0,                                  ///< zigzag_stagger
-    0,                                  ///< opcg_align
-    0,                                  ///< lbist_opcg1
-    {0},                                ///< opcg_count_adjust
-    10000,                              ///< max_polls
-    200000,                             ///< poll_delay_hw
-    1120000,                            ///< poll_delay_sim
-    cc::SCAN_TYPE_ALL,                  ///< scan0_types
-    0,                                  ///< lbist_scan_types
-    cc::REGION_ALL,                     ///< base_regions
-    0,                                  ///< padding
-    {0},                                ///< chiplets_regions
-    0x8000,                             ///< outer_loop_mask
-    0x8000,                             ///< inner_loop_mask
-    "istep",                            ///< program
-    "none",                             ///< ring_patch
-};
-
-enum ODY_ABIST_Private_Constants
-{
-};
-
 ReturnCode ody_abist(const Target<TARGET_TYPE_OCMB_CHIP>& i_target)
 {
-    FAPI_INF("Entering ...");
-
-    ReturnCode l_rc;
-    bist_diags l_bist_diags = {0};
-
-    // Dummy variables to support poz_bist API, but unused in istep mode
-    hwp_data_unit dbuff[128 * 4]; // SCA TODO: implement a filestream instead??
-    hwp_array_ostream l_stream(dbuff, (sizeof(dbuff) / sizeof((dbuff)[0])));
-
-    FAPI_EXEC_HWP(l_rc, poz_bist, i_target, ody_abist_params, l_bist_diags, l_stream, BIST_CHIPOP_ID);
-    FAPI_TRY(l_rc);
-
-    if (l_bist_diags.completed_stages & ody_abist_params.bist_stages::COMPARE)
-    {
-        buffer<uint64_t> l_failing_chiplets_mask = 0;
-
-        for (uint8_t chiplet_id = 0; chiplet_id < 64; chiplet_id++)
-        {
-            if (l_bist_diags.failing_regions[chiplet_id])
-            {
-                l_failing_chiplets_mask.setBit(chiplet_id);
-            }
-        }
-
-        FAPI_ASSERT(l_failing_chiplets_mask == 0,
-                    fapi2::BIST_NONZERO_MISCOMPARES().
-                    set_ABIST_NOT_LBIST(1).
-                    set_FAILING_CHIPLETS_MASK(l_failing_chiplets_mask),
-                    "Chiplets failing compare check: 0x%08x%08x",
-                    l_failing_chiplets_mask >> 32,
-                    l_failing_chiplets_mask & 0xFFFFFFFF);
-    }
-
-fapi_try_exit:
-    FAPI_INF("Exiting ...");
-    return current_err;
+    FAPI_DBG("No product ABIST on Odyssey, skipping...");
+    return FAPI2_RC_SUCCESS;
 }
