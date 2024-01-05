dcfb92576696bbe3e5348dfc9bc730b597a706af
Daniel Kiss <kiss@de.ibm.com>
2024-01-05 15:26:41 +0100

    poz: Silence file not found errors while searching scan image
    Change-Id: I9b88084ff564d879a89600b678d6ce86719d995a
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hw/sbe/+/162820
    Reviewed-by: Daniel Kiss <kiss@de.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>

--- public/src/runtime/common/hwpf/plat_file_access.C
+++ public/src/runtime/common/hwpf/plat_file_access.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER sbe Project                                                  */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2023                             */
+/* Contributors Listed Below - COPYRIGHT 2023,2024                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -38,7 +38,7 @@ ReturnCode plat_loadEmbeddedFile(const char* i_path,
     SBE_ENTER(SBE_FUNC);
 
     ReturnCode l_rc = FAPI2_RC_SUCCESS;
-    uint32_t uncompressedFileSize = 0x00;
+    FileArchive::Entry l_entry;
     ARC_RET_t pakRc = ARC_OPERATION_SUCCESSFUL;
     o_data = NULL;
     o_size = 0x0;
@@ -48,8 +48,7 @@ ReturnCode plat_loadEmbeddedFile(const char* i_path,
         //TODO: JIRA:PFSBE-248 : We could optimize by skipping copying of image if its uncompressed.
         //      But HWP will always call freeembedded API . But we have not allocoated Heap.
         //Locate the file in sppe.ram.pak or pak stack area and fetch the uncompressed file size
-        pakRc = SBE_GLOBAL->embeddedArchive.get_image_start_ptr_and_size(
-                                        i_path, NULL, &uncompressedFileSize);
+        pakRc = SBE_GLOBAL->embeddedArchive.locate_file(i_path, l_entry, ARCHIVE_FLAGS_NOT_FOUND_QUIET);
         if(pakRc ==  ARC_FILE_NOT_FOUND)
         {
            l_rc = FAPI2_RC_FILE_NOT_FOUND;
@@ -65,7 +64,7 @@ ReturnCode plat_loadEmbeddedFile(const char* i_path,
         // File size is already returned above .
         // Use the same for allocoating scratch area
         uint32_t *decompressionScratchArea =
-                    (uint32_t*)Heap::get_instance().scratch_alloc(uncompressedFileSize);
+                    (uint32_t*)Heap::get_instance().scratch_alloc(l_entry.get_size());
         if(decompressionScratchArea == NULL)
         {
             FAPI_ERR(SBE_FUNC "scratch allocation failed. Not enough scratch area to allocate");
@@ -75,7 +74,7 @@ ReturnCode plat_loadEmbeddedFile(const char* i_path,
 
         // Uncompresses/copy the file into the scratch area
         pakRc = SBE_GLOBAL->embeddedArchive.read_file(i_path, decompressionScratchArea,
-                                uncompressedFileSize, NULL, NULL);
+                                l_entry.get_size(), NULL, NULL);
         if( pakRc != ARC_OPERATION_SUCCESSFUL )
         {
             FAPI_ERR(SBE_FUNC "File read failed with RC[%08X]", pakRc);
@@ -88,7 +87,7 @@ ReturnCode plat_loadEmbeddedFile(const char* i_path,
         }
 
         o_data = decompressionScratchArea;
-        o_size = uncompressedFileSize;
+        o_size = l_entry.get_size();
     }while(false);
 
     SBE_EXIT(SBE_FUNC);
