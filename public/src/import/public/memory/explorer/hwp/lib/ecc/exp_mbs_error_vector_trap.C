6b4667d856809e258365dbc5a43b90b53ce8d404
Geetha Pisapati <Geetha.Pisapati@ibm.com>
2024-01-08 17:03:23 -0500

    Adds per-port error vector trap in ody_memory_ecc_decode
    Change-Id: I49a5c7b17a9214d93c9c672103e157669ce51cd2
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hw/ekb-src/+/163001
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: David-Jude Chung <dj.chung@ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Amita Banchhor <amita.banchhor@ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hw/sbe/+/163005
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Reviewed-by: Team <hostboot@us.ibm.com>

---
+++ public/src/import/public/memory/explorer/hwp/lib/ecc/exp_mbs_error_vector_trap.C
@@ -0,0 +1,120 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: public/src/import/public/memory/explorer/hwp/lib/ecc/exp_mbs_error_vector_trap.C $ */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2024                             */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+///
+/// @file exp_mbs_error_vector_trap.C
+/// @brief  Main functions for ecc decode
+///
+// *HWP HWP Owner: Geetha Pisapati <geetha.pisapati@ibm.com>
+// *HWP HWP Backup: Louis Stermole <stermole@us.ibm.com>
+// *HWP Team: Memory
+// *HWP Level: 2
+// *HWP Consumed by: Memory
+
+#include <lib/mcbist/exp_mcbist_traits.H>
+#include <lib/ecc/ecc_traits_explorer.H>
+#include <explorer_scom_addresses.H>
+#include <explorer_scom_addresses_fld.H>
+#include <generic/memory/lib/ecc/ecc.H>
+#include <lib/ecc/ecc_traits_explorer.H>
+#include <generic/memory/lib/ecc/mbs_error_vector_trap.H>
+#include <lib/dimm/exp_rank.H>
+
+namespace mss
+{
+namespace ecc
+{
+namespace mbs_error_vector_trap
+{
+
+///
+/// @brief get_nce_galois
+/// @param[in] i_target the ocmb target
+/// @param[in] i_data the register value
+/// @param[out] o_value the value of the field
+///
+template<>
+void get_nce_galois<mss::mc_type::EXPLORER>( const fapi2::Target<fapi2::TARGET_TYPE_MEM_PORT>& i_target,
+        const fapi2::buffer<uint64_t>& i_data,
+        uint64_t& o_value)
+{
+    using TT = eccTraits<mss::mc_type::EXPLORER, fapi2::TARGET_TYPE_MEM_PORT>;
+
+    i_data.extractToRight<TT::P0_NCE_GALOIS, TT::P0_NCE_GALOIS_LEN>(o_value);
+}
+
+//
+/// @brief get_nce_magnitude
+/// @param[in] i_target the ocmb target
+/// @param[in] i_data the register value
+/// @param[out] o_value the value of the field
+///
+template<>
+void get_nce_magnitude<mss::mc_type::EXPLORER>( const fapi2::Target<fapi2::TARGET_TYPE_MEM_PORT>& i_target,
+        const fapi2::buffer<uint64_t>& i_data,
+        uint64_t& o_value)
+{
+    using TT = eccTraits<mss::mc_type::EXPLORER, fapi2::TARGET_TYPE_MEM_PORT>;
+
+    i_data.extractToRight<TT::P0_NCE_MAGNITUDE, TT::P0_NCE_MAGNITUDE_LEN>(o_value);
+}
+
+///
+/// @brief get_tce_galois
+/// @param[in] i_target the ocmb target
+/// @param[in] i_data the register value
+/// @param[out] o_value the value of the field
+///
+template<>
+void get_tce_galois<mss::mc_type::EXPLORER>( const fapi2::Target<fapi2::TARGET_TYPE_MEM_PORT>& i_target,
+        const fapi2::buffer<uint64_t>& i_data,
+        uint64_t& o_value)
+{
+    using TT = eccTraits<mss::mc_type::EXPLORER, fapi2::TARGET_TYPE_MEM_PORT>;
+
+    i_data.extractToRight<TT::P0_TCE_GALOIS, TT::P0_TCE_GALOIS_LEN>(o_value);
+}
+
+
+///
+/// @brief get_tce_magnitude
+/// @param[in] i_target the ocmb target
+/// @param[in] i_data the register value
+/// @param[out] o_value the value of the field
+///
+template<>
+void get_tce_magnitude<mss::mc_type::EXPLORER>( const fapi2::Target<fapi2::TARGET_TYPE_MEM_PORT>& i_target,
+        const fapi2::buffer<uint64_t>& i_data,
+        uint64_t& o_value)
+{
+
+    using TT = eccTraits<mss::mc_type::EXPLORER, fapi2::TARGET_TYPE_MEM_PORT>;
+
+    i_data.extractToRight<TT::P0_TCE_MAGNITUDE, TT::P0_TCE_MAGNITUDE_LEN>(o_value);
+}
+
+}// ns mbs_error_vector_trap
+}// ns ecc
+}// ns
