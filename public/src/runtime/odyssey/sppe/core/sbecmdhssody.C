d64b54ee1a58f184967d80c90f64a6589a79d45c
Josh-Chica <Josh.Chica@ibm.com>
2024-01-29 11:10:37 -0500

    Updated Ody IO Chipop
    - Renamed chipop to be more generic for HSS
    - Added chipop for Ody TDR
    
    Change-Id: Ica2bbdb92cb86efef06551a55b4a84c435359a67
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hw/sbe/+/164260
    Reviewed-by: Chris Steffen <cwsteffen@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>

--- public/src/runtime/odyssey/sppe/core/sbecmdhssody.C
+++ public/src/runtime/odyssey/sppe/core/sbecmdhssody.C
@@ -1,12 +1,11 @@
 /* IBM_PROLOG_BEGIN_TAG                                                   */
 /* This is an automatically generated prolog.                             */
 /*                                                                        */
-/* $Source: public/src/runtime/odyssey/sppe/core/sbecmdodyunload.C $      */
+/* $Source: public/src/runtime/odyssey/sppe/core/sbecmdhssody.C $         */
 /*                                                                        */
 /* OpenPOWER sbe Project                                                  */
 /*                                                                        */
 /* Contributors Listed Below - COPYRIGHT 2023,2024                        */
-/* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
 /* Licensed under the Apache License, Version 2.0 (the "License");        */
@@ -22,7 +21,7 @@
 /* permissions and limitations under the License.                         */
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
-#include "sbecmdodyunload.H"
+#include "sbecmdhssody.H"
 #include "sbefifo.H"
 #include "sbe_sp_intf.H"
 #include "sbetrace.H"
@@ -37,6 +36,7 @@
 #include "plat_hwp_data_stream.H"
 #include <stdint.h>
 
+#include <ody_omi_tdr.H>
 #include <ody_omi_unload.H>
 
 using namespace fapi2;
@@ -72,6 +72,70 @@ uint32_t sbeHssOdyUnload(uint8_t *i_pArg)
         // Call the wrapper
         fapi2::ReturnCode fapi_rc = fapi2::FAPI2_RC_SUCCESS;
         SBE_EXEC_HWP( fapi_rc, ody_omi_unload, tgt, c_chipunit_mask, l_bit_os);
+        if (fapi_rc != fapi2::FAPI2_RC_SUCCESS)
+        {
+            hdr.setStatus(SBE_PRI_GENERIC_EXECUTION_FAILURE, SBE_SEC_GENERIC_FAILURE_IN_EXECUTION);
+            ffdc.setRc(fapi_rc);
+            break;
+        }
+    } while(false);
+
+    if (l_rc != SBE_SEC_OPERATION_SUCCESSFUL)
+    {
+        if (needsDrain)
+        {
+            uint32_t len = 0;
+            uint32_t rc = sbeUpFifoDeq_mult(len, l_params, true, true, type);
+            if (rc != SBE_SEC_OPERATION_SUCCESSFUL) // FIFO access issue
+            {
+                SBE_ERROR(SBE_FUNC "FIFO EOT flush failed, rc[0x%X]", rc);
+            }
+        }
+
+        hdr.setStatus(SBE_PRI_GENERIC_EXECUTION_FAILURE, l_rc);
+    }
+    do
+    {
+        SBE_TRY(sbeDsSendRespHdr(hdr, &ffdc, type));
+    } while (0);
+
+    SBE_EXIT(SBE_FUNC);
+    return l_rc;
+    #undef SBE_FUNC
+
+}
+
+/////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+uint32_t sbeHssOdyTdr(uint8_t *i_pArg)
+{
+#define SBE_FUNC " sbeHssOdyTdr "
+    SBE_ENTER(SBE_FUNC);
+
+    constexpr uint32_t c_chipunit_mask = 0;
+
+    uint32_t l_rc = SBE_SEC_OPERATION_SUCCESSFUL;
+    sbeRespGenHdr_t hdr;
+    hdr.init();
+    sbeResponseFfdc_t ffdc;
+    sbeFifoType type;
+    uint32_t l_params[64];
+    bool needsDrain = true;
+
+    Target<SBE_ROOT_CHIP_TYPE> tgt = g_platTarget->plat_getChipTarget();
+
+    do
+    {
+        chipOpParam_t* configStr = (struct chipOpParam*)i_pArg;
+        type = static_cast<sbeFifoType>(configStr->fifoType);
+        SBE_DEBUG(SBE_FUNC "Fifo Type is:[%02X]",type);
+        needsDrain = false;
+
+        fapi2::sbefifo_hwp_data_ostream l_ostream(type);
+        hwp_bit_ostream l_bit_os(l_ostream);
+        // Call the wrapper
+        fapi2::ReturnCode fapi_rc = fapi2::FAPI2_RC_SUCCESS;
+        SBE_EXEC_HWP( fapi_rc, ody_omi_tdr, tgt, c_chipunit_mask, l_bit_os);
         if (fapi_rc != fapi2::FAPI2_RC_SUCCESS)
         {
             hdr.setStatus(SBE_PRI_GENERIC_EXECUTION_FAILURE, SBE_SEC_GENERIC_FAILURE_IN_EXECUTION);
