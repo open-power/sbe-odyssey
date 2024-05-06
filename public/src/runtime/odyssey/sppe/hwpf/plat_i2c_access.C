9553210b6de427152a7bbf10d9ce3bec1fabfc24
RAJESWARAN THILLAIGOVINDAN <rajeswgo@in.ibm.com>
2024-05-06 05:54:26 -0500

    Modified i2c operation to return the first fail only
    In case of i2c operation failure, multiple recoverable error logs are created.
    Since thermal sensor polling runs for every 200ms, there will be a flood of
    recoverable error logs. Because of this, hostboot buffer went out of space.
    To avoid this issue, only one error log will be created in case of i2c
    operation failure.
    
    Change-Id: I3ec1af02c81102084a42ce5a74debc925864de77
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hw/sbe/+/171225
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>
    Reviewed-by: Rangaswamy H B <rangaswamy.h.b@ibm.com>
    Reviewed-by: Akhilesh S <akhis023@in.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>

--- public/src/runtime/odyssey/sppe/hwpf/plat_i2c_access.C
+++ public/src/runtime/odyssey/sppe/hwpf/plat_i2c_access.C
@@ -71,4 +71,116 @@ ReturnCode plati2c::populatei2cdetails(const Target<TARGET_TYPE_ALL>& target)
         return current_err;
 }
 
+ReturnCode plati2c::getI2c( const Target<TARGET_TYPE_ALL>& target,
+                            const size_t get_size,
+                            const std::vector<uint8_t>& cfgData,
+                            std::vector<uint8_t>& o_data )
+{
+    #define SBE_FUNC " getI2c "
+    SBE_ENTER(SBE_FUNC)
+
+    ReturnCode rc = FAPI2_RC_SUCCESS;
+    do{
+        //We just need to populate i2c details once
+        rc = populatei2cdetails(target);
+        if(rc != FAPI2_RC_SUCCESS)
+        {
+            SBE_ERROR(SBE_FUNC " failed for populatei2cdetails with rc 0x%08X", rc);
+            break;
+        }
+
+        do{
+            rc = i2cLockEngine();
+            if(rc != FAPI2_RC_SUCCESS)
+            {
+                SBE_ERROR(SBE_FUNC " failed for i2cLockEngine with rc 0x%08X", rc);
+                break;
+            }
+
+            SBE_DEBUG(SBE_FUNC "Current retry count 0x%02x", iv_curr_retry_count);
+            rc = getI2cHelper(get_size, cfgData, o_data);
+            //Lets exit out if there were no i2c fails
+            if (rc == FAPI2_RC_SUCCESS)
+            {
+                break;
+            }
+            else
+            {
+                // if getI2cHelper failed try to reset
+                ReturnCode l_rc = i2cReset();
+                if(l_rc != FAPI2_RC_SUCCESS)
+                {
+                    break;
+                }
+            }
+            iv_curr_retry_count++;
+
+        }while(iv_curr_retry_count <= iv_max_retry_count);
+    }while(false);
+    ReturnCode l_rc = i2cUnlockEngine();
+    if(l_rc != FAPI2_RC_SUCCESS)
+    {
+        SBE_ERROR(SBE_FUNC " failed for i2cUnlockEngine with l_rc 0x%08X", l_rc);
+    }
+    SBE_EXIT(SBE_FUNC)
+    return rc;
+    #undef SBE_FUNC
+}
+
+ReturnCode plati2c::putI2c( const Target<TARGET_TYPE_ALL>& target,
+                            const std::vector<uint8_t>& data )
+{
+    #define SBE_FUNC " putI2c "
+    SBE_ENTER(SBE_FUNC)
+
+    ReturnCode rc = FAPI2_RC_SUCCESS;
+
+    do{
+        //We just need to populate i2c details once
+        rc = populatei2cdetails(target);
+        if(rc != FAPI2_RC_SUCCESS)
+        {
+            SBE_ERROR(SBE_FUNC " failed for populatei2cdetails with rc 0x%08X", rc);
+            break;
+        }
+
+        do{
+            rc = i2cLockEngine();
+            if(rc != FAPI2_RC_SUCCESS)
+            {
+                SBE_ERROR(SBE_FUNC " failed for i2cLockEngine with rc 0x%08X", rc);
+                break;
+            }
+
+            SBE_DEBUG(SBE_FUNC "Current retry count 0x%02x", iv_curr_retry_count);
+            rc = putI2cHelper(data);
+            //Lets exit out if there were no i2c fails
+            if (rc == FAPI2_RC_SUCCESS)
+            {
+                break;
+            }
+            else
+            {
+                // if putI2cHelper failed
+                // try to reset
+                ReturnCode l_rc = i2cReset();
+                if(l_rc != FAPI2_RC_SUCCESS)
+                {
+                    break;
+                }
+            }
+            iv_curr_retry_count++;
+
+        }while(iv_curr_retry_count <= iv_max_retry_count);
+    }while(false);
+    ReturnCode l_rc = i2cUnlockEngine();
+    if(l_rc != FAPI2_RC_SUCCESS)
+    {
+        SBE_ERROR(SBE_FUNC " failed for i2cUnlockEngine with l_rc 0x%08X", l_rc);
+    }
+    SBE_EXIT(SBE_FUNC)
+    return rc;
+    #undef SBE_FUNC
+}
+
 } //namespace fapi2
